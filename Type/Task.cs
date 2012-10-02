980037c8072469fe4aa8fb201683c8d506fa5d37
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-10-03 03:17:55 +0800

    Skeleton files. Please commit to 'dev' from now on.

---
+++ Type/Task.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Type
+{
+    class Task
+    {
+        private string rawText;
+
+        private bool done;
+        private bool archive;
+
+        private DateTime start;
+        private DateTime end;
+
+        private List<string> tags;
+
+        private List<Tuple<string, int>> tokens;
+
+        public string RawText
+        {
+            get
+            {
+                return rawText;
+            }
+        }
+
+        public bool Done
+        {
+            get
+            {
+                return done;
+            }
+
+            set
+            {
+                done = value;
+            }
+        }
+
+        public bool Archive
+        {
+            get
+            {
+                return archive;
+            }
+
+            set
+            {
+                archive = value;
+            }
+        }
+
+        public DateTime Start
+        {
+            get
+            {
+                return start;
+            }
+        }
+
+        public DateTime End
+        {
+            get
+            {
+                return end;
+            }
+        }
+
+        public IList<string> Tags
+        {
+            get
+            {
+                return tags.AsReadOnly();
+            }
+        }
+
+        public IList<Tuple<string, int>> Tokens
+        {
+            get
+            {
+                return tokens.AsReadOnly();
+            }
+        }
+
+        public Task(string rawText)
+        {
+            // @michael TODO
+            
+
+        }
+    }
+}
