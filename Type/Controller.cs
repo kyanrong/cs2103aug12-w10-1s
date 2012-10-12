3fafc7d7322552c66b21e55f549261e8bdf2ad7d
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-10-12 13:04:04 +0800

    Execute command now works. :edit now removes a task from the list (but still does not return the raw text to the UI.

---
+++ Type/Controller.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+
+namespace Type
+{
+    internal delegate void UIRedrawHandler(IList<Task> updateData);
+
+    public class Controller
+    {
+        private string COMMAND_PREFIX = ":";
+        private Key[] START_KEY_COMBINATION = { Key.LeftShift, Key.Space };
+
+        private GlobalKeyCombinationHook globalHook;
+        private MainWindow ui;
+
+        private List<Task> tasks;
+        private AutoComplete tasksAutoComplete;
+
+        public Controller()
+        {
+            //Sequence is important here. We need to initialize backend storage first,
+            //followed by instantiating the UI, and finally, listening on the keyboard
+            //hook. Messing up the sequence may result in race conditions.
+
+            tasks = new List<Task>();
+            tasksAutoComplete = new AutoComplete();
+
+            ui = (new MainWindow()).setCallbacks(ExecuteCommand, GetTaskSuggestions);
+
+            globalHook = new GlobalKeyCombinationHook(ShowUi, START_KEY_COMBINATION);
+        }
+
+        ~Controller()
+        {
+            globalHook.StopListening();
+        }
+
+        private void ShowUi()
+        {
+            ui.Show();
+        }
+
+        private string ExtractCommandToken(ref string userInput)
+        {
+            int spIndex = userInput.IndexOf(' ');
+            string commandToken = userInput.Substring(1, spIndex - 1);
+            userInput = userInput.Substring(spIndex + 1);
+            return commandToken;
+        }
+
+        private bool IsDefaultCommand(string userInput)
+        {
+            return (!userInput.StartsWith(COMMAND_PREFIX));
+        }
+
+        internal void ExecuteCommand(string userInput, UIRedrawHandler redrawHandler)
+        {
+            //The default command is 'add'
+            if (IsDefaultCommand(userInput))
+            {
+                tasks.Add(new Task(userInput));
+                tasksAutoComplete.AddSuggestion(userInput);
+            }
+            else
+            {
+                string command = ExtractCommandToken(ref userInput);
+                Task selectedTask = FindTaskByText(userInput);
+                switch (command)
+                {
+                    case "done":
+                        selectedTask.Done = true;
+                        break;
+
+                    case "archive":
+                        selectedTask.Archive = true;
+                        break;
+
+                    case "edit":
+                        tasks.Remove(selectedTask);
+                        tasksAutoComplete.RemoveSuggestion(selectedTask.RawText);
+                        break;
+                }
+            }
+            redrawHandler(tasks.AsReadOnly());
+        }
+
+        private Task FindTaskByText(string rawText)
+        {
+            return (tasks.First(task => task.RawText == rawText));
+        }
+
+        private IList<Task> GetTasksToDisplay(int count = 5, List<string> tags = null)
+        {
+            if (tags == null)
+            {
+                return (tasks.Take(count).ToList().AsReadOnly());
+            }
+            else
+            {
+                var resultSet = new HashSet<Task>();
+                foreach (var tag in tags)
+                {
+                    resultSet.UnionWith(tasks.Where(t => t.Tags.Contains(tag)));
+                }
+                return (resultSet.Take(count).ToList().AsReadOnly());
+            }
+        }
+
+        private IAutoComplete GetTaskSuggestions()
+        {
+            return tasksAutoComplete;
+        }
+    }
+}
---
+++ Type/GlobalKeyCombinationHook.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+
+namespace Type
+{
+    internal delegate void UIDisplayHandler();
+
+    class GlobalKeyCombinationHook
+    {
+        private delegate IntPtr GlobalKeypressEventCallback(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private const int KEYPRESS_DELAY = 200;
+
+        private GlobalKeypressEventCallback callback;
+        private IntPtr hookId = IntPtr.Zero;
+
+        private UIDisplayHandler UIHandler;
+
+        private HashSet<Keys> requiredCombination;
+        private HashSet<Keys> currentCombination;
+        private DateTime firstKey;
+        private bool hasKeys;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook, GlobalKeypressEventCallback lpfn, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        private static IntPtr SetHook(GlobalKeypressEventCallback proc)
+        {
+            var curProcess = Process.GetCurrentProcess();
+            var curModule = curProcess.MainModule;
+            return SetWindowsHookEx(WH_KEYBOARD_LL, proc, GetModuleHandle(curModule.ModuleName), 0);
+        }
+
+        private IntPtr GlobalKeypressEventHandler(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (IsCallback(nCode, wParam))
+            {
+                var vkCode = Marshal.ReadInt32(lParam);
+                Keys pressed = (Keys)vkCode;
+
+                if (requiredCombination.Contains(pressed))
+                {
+                    ProcessValidKeypress(pressed);
+                }
+                else
+                {
+                    ResetCurrentCombination();
+                }
+            }
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+
+        private void ProcessValidKeypress(Keys pressed)
+        {
+            currentCombination.Add(pressed);
+
+            if (hasKeys)
+            {
+                ProcessCombination();
+            }
+            else
+            {
+                RecordFirstValidKeypress();
+            }
+        }
+
+        private void ProcessCombination()
+        {
+            if (IsRequiredCombination())
+            {
+                if (IsWithin(KEYPRESS_DELAY, firstKey))
+                {
+                    UIHandler();
+                }
+                else
+                {
+                    ResetCurrentCombination();
+                }
+            }
+        }
+
+        private bool IsRequiredCombination()
+        {
+            return currentCombination.Count == requiredCombination.Count;
+        }
+
+        private void RecordFirstValidKeypress()
+        {
+            firstKey = DateTime.Now;
+            hasKeys = true;
+        }
+
+        private bool IsWithin(int delay, DateTime time)
+        {
+            TimeSpan elapsed = DateTime.Now - time;
+            var limit = new TimeSpan(0, 0, 0, 0, delay);
+            return elapsed <= limit;
+        }
+
+        private void ResetCurrentCombination()
+        {
+            currentCombination.Clear();
+            hasKeys = false;
+        }
+
+        private static bool IsCallback(int nCode, IntPtr wParam)
+        {
+            return nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN;
+        }
+
+        public GlobalKeyCombinationHook(UIDisplayHandler UIHandler, Key[] combination)
+        {
+            callback = GlobalKeypressEventHandler;
+
+            this.UIHandler = UIHandler;
+            requiredCombination = new HashSet<Keys>();
+            currentCombination = new HashSet<Keys>();
+            hasKeys = false;
+
+            BuildRequiredKeyCombinationSet(combination);
+
+            StartListening();
+        }
+
+        private void BuildRequiredKeyCombinationSet(Key[] combination)
+        {
+            foreach (Key key in combination)
+            {
+                requiredCombination.Add((Keys)KeyInterop.VirtualKeyFromKey(key));
+            }
+        }
+
+        private void StartListening()
+        {
+            hookId = GlobalKeyCombinationHook.SetHook(callback);
+        }
+
+        public void StopListening()
+        {
+            GlobalKeyCombinationHook.UnhookWindowsHookEx(hookId);
+        }
+    }
+}
