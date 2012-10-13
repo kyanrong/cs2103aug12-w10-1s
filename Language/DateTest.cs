e4348f04a0e94b34c336cef651a64135d47ecdf8
Yong Michael <wrong92@gmail.com>
2012-10-13 10:47:54 +0800

    more tests

---
+++ Language/DateTest.cs
@@ -0,0 +1,82 @@
+﻿using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Language
+{
+    [TestClass]
+    public class DateTest
+    {
+        [TestMethod]
+        public void NegativeTests()
+        {
+            List<string> l = new List<string>();
+            l.Add("This should not match");
+            l.Add("This 12 13 13 should not match");
+            l.Add("This 12112 should not match");
+            l.Add("This January");
+
+            foreach (string x in l)
+            {
+                Assert.AreEqual(RegExp.Date(x), string.Empty);
+            }
+        }
+
+        [TestMethod]
+        public void dd_m()
+        {
+            string x = "This should 12/1 match";
+            Assert.AreEqual(RegExp.Date(x), "12/1");
+        }
+
+        [TestMethod]
+        public void dd_mm()
+        {
+            string x = "This should 12/11 match";
+            Assert.AreEqual(RegExp.Date(x), "12/11");
+        }
+
+        [TestMethod]
+        public void dd_mm_yy()
+        {
+            string x = "This should 12/11/11 match";
+            Assert.AreEqual(RegExp.Date(x), "12/11/11");
+        }
+
+        [TestMethod]
+        public void dd_mm_yyyy()
+        {
+            string x = "This should 12/11/2011 match";
+            Assert.AreEqual(RegExp.Date(x), "12/11/2011");
+        }
+
+        [TestMethod]
+        public void dd_month()
+        {
+            string[] months = {
+               "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
+               "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+            };
+            foreach (string month in months)
+            {
+                string x = "This should 11 " + month + " match";
+                Assert.AreEqual(RegExp.Date(x), "11 " + month);
+            }
+        }
+
+        [TestMethod]
+        public void dd_month_yyyy()
+        {
+            string x = "This should 12 march 2012 match";
+            Assert.AreEqual(RegExp.Date(x), "12 march 2012");
+        }
+
+        [TestMethod]
+        public void dd_month_yy()
+        {
+            string x = "This should 12 march 12 match";
+            Assert.AreEqual(RegExp.Date(x), "12 march 12");
+        }
+    }
+}
---
+++ Type/Task.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Type
+{
+    class Task
+    {
+        // Store's the user's raw input
+        private string rawText;
+        
+        // Standard Properties of all tasks
+        private int id;
+        private bool done;
+        private bool archive;
+
+        // Parsed Properties
+        private DateTime start;
+        private DateTime end;
+        private List<string> tags;
+        
+        // Parsed Types
+        private static enum types {
+            String,
+            Hashtag,
+            Datetime
+        };
+
+        private List<Tuple<string, types>> tokens;
+
+        // Constructor
+        public Task(string rawText)
+        {
+            // saves the rawText
+            // parsing should be idempotent
+            // re-parsing on the same rawText
+            // should return the same values
+            this.rawText = rawText;
+
+           // default values
+           this.done = false;
+           this.archive = false;
+
+           // parse the input
+           this.parse();
+        }
+
+        private void parse()
+        {
+            List<Tuple<string, types>> result = new List<Tuple<string, types>>();
+
+            // TMP.
+            // TODO.
+            Tuple<string, types> t = Tuple.Create(this.rawText, types.String);
+            result.Add(t);
+            
+            // Set tokens
+            this.tokens = result;
+        }
+
+        public bool Done
+        {
+            get { return done; }
+            set { done = value; }
+        }
+        public bool Archive 
+        {
+            get { return archive; }
+            set { archive = value; }
+        }
+
+        // only getters
+        public DateTime Start
+        {
+            get { return start; }
+        }
+        public DateTime End
+        {
+            get { return end; }
+        }
+        public IList<string> Tags
+        {
+            get { return tags.AsReadOnly(); }
+        }
+        public IList<Tuple<string, types>> Tokens
+        {
+            get { return tokens.AsReadOnly(); }
+        }
+        public string RawText
+        {
+            get { return rawText; }
+        }
+        public int Id
+        {
+            get { return id; }
+        }
+
+        public override string ToString()
+        {
+            return rawText;
+        }
+    }
+}
