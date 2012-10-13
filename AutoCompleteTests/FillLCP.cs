bab14f2851c9e997036ed619efa323daf12dbbe8
Ang Family <Ang Family@AngFamily-PC.comp.nus.edu.sg>
2012-10-13 12:17:01 +0800

    implement type file but not yet check

---
+++ AutoCompleteTests/FillLCP.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Type;
+
+namespace AutoCompleteTests
+{
+    [TestClass]
+    public class FillLCP
+    {
+        [TestMethod]
+        public void FillsNothingIfNoMatch()
+        {
+            //Arrange
+            string[] dict = { "catnip", "weed", "ice", "wraith" };
+            AutoComplete ac = new AutoComplete(dict);
+
+            //Act
+            string result = ac.CompleteToCommonPrefix("a");
+
+            //Assert
+            Assert.AreEqual(result, "");
+        }
+
+        [TestMethod]
+        public void FillsWholeStringIfOneMatch()
+        {
+            //Arrange
+            string[] dict = { "catnip", "weed", "ice", "wraith" };
+            AutoComplete ac = new AutoComplete(dict);
+
+            //Act
+            string result = ac.CompleteToCommonPrefix("c");
+
+            //Assert
+            Assert.AreEqual(result, "atnip");
+        }
+
+        [TestMethod]
+        public void FillsToLongestCommonPrefix()
+        {
+            //Arrange
+            string[] dict = { "catnip", "weed", "ice", "wraith", "warzone", "wrong", "cat" };
+            AutoComplete ac = new AutoComplete(dict);
+
+            //Act
+            string result = ac.CompleteToCommonPrefix("c");
+
+            //Assert
+            Assert.AreEqual(result, "at");
+        }
+    }
+}
---
+++ AutoCompleteTests/Object.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Type;
+
+namespace AutoCompleteTests
+{
+    [TestClass]
+    public class Object
+    {
+        [TestMethod]
+        public void CanInstantiate()
+        {
+            //Arrange
+            string[] dict = { "adolph", "bucket sort", "charlie" };
+
+            //Act
+            AutoComplete ac = new AutoComplete(dict);
+
+            //Assert
+            Assert.IsNotNull(ac);
+        }
+
+        [TestMethod]
+        public void DictionaryBuiltSuccessfullyOnConstructor()
+        {
+            //Arrange
+            string[] dict = { "adolph", "bucket sort", "charlie" };
+
+            //Act
+            AutoComplete ac = new AutoComplete(dict);
+
+            //Assert
+            Assert.IsTrue(ac.ContainsSuggestion("adolph"));
+            Assert.IsTrue(ac.ContainsSuggestion("bucket sort"));
+            Assert.IsTrue(ac.ContainsSuggestion("charlie"));
+            Assert.AreEqual(dict.Length, ac.DictionarySize);
+        }
+
+        [TestMethod]
+        public void DictionaryCanAddWords()
+        {
+            //Arrange
+            string[] dict = { "adolph", "bucket sort", "charlie" };
+            AutoComplete ac = new AutoComplete(dict);
+            int originalSize = ac.DictionarySize;
+
+            //Act
+            ac.AddSuggestion("dominos");
+            int newSize = ac.DictionarySize;
+
+            //Assert
+            Assert.IsTrue(ac.ContainsSuggestion("dominos"));
+            Assert.AreEqual(newSize, (originalSize + 1));
+        }
+
+        [TestMethod]
+        public void DictionaryCanRemoveWords()
+        {
+            //Arrange
+            string[] dict = { "adolph", "bucket sort", "charlie" };
+            AutoComplete ac = new AutoComplete(dict);
+            int originalSize = ac.DictionarySize;
+
+            //Act
+            ac.RemoveSuggestion("adolph");
+            int newSize = ac.DictionarySize;
+
+            //Assert
+            Assert.IsFalse(ac.ContainsSuggestion("adolph"));
+            Assert.IsTrue(ac.ContainsSuggestion("bucket sort"));
+            Assert.IsTrue(ac.ContainsSuggestion("charlie"));
+            Assert.AreEqual(newSize, (originalSize - 1));
+        }
+    }
+}
