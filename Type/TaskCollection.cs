c981626315d9b34a618167583367805e421d9213
Ang Civics <angcivics@gmail.com>
2012-10-13 15:36:35 +0800

    data store method all checked, should be working

---
+++ Type/TaskCollection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Type
+{
+    public class TaskCollection
+    {
+        private List<Task> tasks;
+        private DataStore dataStore;
+
+        // Constructor
+        public TaskCollection()
+        {
+            // instantiate task models
+            tasks = new List<Task>();
+            // create data store
+            dataStore = new DataStore("taskcollection.csv");
+
+            // load flat file into memory.
+            this.Fetch();
+        }
+
+        // Fetch Tasks from Flatfile
+        public void Fetch()
+        {
+            // TODO.
+        }
+
+        // Create Task
+        public Task Create(string input)
+        {
+            Task t = new Task(input);
+            tasks.Add(t);
+            return t;
+        }
+
+        // Get Task
+        public Task Get(int id)
+        {
+            return tasks.Find(task => task.Id == id);
+        }
+
+        // Helper Methods
+        // Filter All
+        public List<Task> filterAll(string input)
+        {
+            return tasks.FindAll(
+                task => task.RawText.StartsWith(input)
+            );
+        }
+    }
+}
