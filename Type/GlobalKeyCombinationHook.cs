27942a78f72152f9376036576d17723b614c9cf3
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-10-13 09:54:17 +0800

    Documented keyboard hook class.

--- Type/GlobalKeyCombinationHook.cs
+++ Type/GlobalKeyCombinationHook.cs
@@ -12,16 +12,25 @@ namespace Type
     {
         private const string ATOM_NAME = "TypeShortcut";
 
+        //Handles and references to a System.Windows.Window for listening to system messages.
         private Window targetWindow;
-
         private HwndSource targetWindowHandle;
-        private short atom;
+        private short atom;    
 
-        private KeyCombinationHandler combinationHandler;
-
+        //Key combination to catch.
         private uint modifier;
         private uint vkey;
 
+        //Callback to execute if the key combination is caught.
+        private KeyCombinationHandler combinationHandler;
+
+        /// <summary>
+        /// Creates a GlobalKeyCombination Hook object.
+        /// </summary>
+        /// <param name="targetWindow">Reference to a System.Windows.Window.</param>
+        /// <param name="combinationHandler">Callback that receives key combination messages.</param>
+        /// <param name="modifier">Represents the combination of modifier keys to catch.</param>
+        /// <param name="vkey">Represents the VIRTUAL_KEY value fo the key to catch.</param>
         public GlobalKeyCombinationHook(Window targetWindow, KeyCombinationHandler combinationHandler, uint modifier, uint vkey)
         {
             this.targetWindow = targetWindow;
@@ -31,6 +40,10 @@ namespace Type
             this.vkey = vkey;
         }
 
+        /// <summary>
+        /// Creates a keyboard hook and starts listening.
+        /// </summary>
+        /// <returns>Self-reference.</returns>
         public GlobalKeyCombinationHook StartListening()
         {
             //The window only gets a hwnd after it is shown the first time.
@@ -52,6 +65,7 @@ namespace Type
             return this;
         }
 
+        //Restores the visual state of a System.Windows.Window.
         private void RestoreProperties(Window win, Tuple<double, double, WindowStyle, bool, bool> prop)
         {
             win.Height = prop.Item1;
@@ -61,6 +75,7 @@ namespace Type
             win.ShowActivated = prop.Item5;
         }
 
+        //Makes a System.Windows.Window visually invisible.
         private void MakeInvisible(Window win)
         {
             win.Height = 0;
@@ -70,6 +85,7 @@ namespace Type
             win.ShowActivated = false;
         }
 
+        //Temporarily stores the visual state of a System.Windows.Window.
         private Tuple<double, double, WindowStyle, bool, bool> StoreProperties(Window win)
         {
             double height = targetWindow.Height;
@@ -81,6 +97,10 @@ namespace Type
             return new Tuple<double, double, WindowStyle, bool, bool>(height, width, style, taskbar, activation);
         }
 
+        /// <summary>
+        /// Removes the keyboard hook.
+        /// </summary>
+        /// <returns>Self reference.</returns>
         public GlobalKeyCombinationHook StopListening()
         {
             UnregisterCombination();
@@ -88,6 +108,7 @@ namespace Type
             return this;
         }
 
+        //Unregister a previously registered atom with the host OS.
         private void UnregisterCombination()
         {
             if (atom != 0)
@@ -96,6 +117,7 @@ namespace Type
             }
         }
 
+        //Add a hook to listen to messages from the host OS.
         private WindowInteropHelper AddHook()
         {
             var wih = new WindowInteropHelper(targetWindow);
@@ -106,6 +128,7 @@ namespace Type
             return wih;
         }
 
+        //Registers the combination that was initialized with this object with the host OS.
         private void RegisterCombination(WindowInteropHelper wih)
         {
             bool result = RegisterHotKey(wih.Handle, atom, modifier, vkey);
@@ -116,6 +139,7 @@ namespace Type
             }
         }
 
+        //Create an atom in win32 to receive system messages.
         private short CreateAtom()
         {
             short atom = GlobalAddAtom(ATOM_NAME);
@@ -128,6 +152,8 @@ namespace Type
             return atom;
         }
 
+        //Handles system messages.
+        //Calls the callback that was initialized with this object if the message is WM_HOTKEY.
         private IntPtr HotKeyMsgHandler(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
         {
             if (msg == GlobalKeyCombinationHook.WM_HOTKEY)
