bcd3481d777440cbfcdd6fb38392533695201dbe
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-10-16 20:41:08 +0800

    Extracted parse function to static member of Commands.

--- Type/Commands.cs
+++ Type/Commands.cs
@@ -16,5 +16,40 @@ namespace Type
 
         public const string SearchToken = "/";
         public const string Search = "search";
+
+        /// <summary>
+        /// Parses input by splitting it into a token containing the command's text, and a token containing the rest of the input.
+        /// </summary>
+        /// <param name="input">Input to parse. Commands should start with the symbol defined in COMMAND_TOKEN.</param>
+        /// <returns>A Tuple containing the command text and remaining input.</returns>
+        public static Tuple<string, string> Parse(string input)
+        {
+            string cmd;
+            if (input.StartsWith(Commands.Token))
+            {
+                int spIndex = input.IndexOf(' ');
+                if (spIndex < 0)
+                {
+                    cmd = Commands.Invalid;
+                    input = "";
+                }
+                else
+                {
+                    cmd = input.Substring(1, spIndex - 1);
+                    input = input.Substring(spIndex + 1);
+                }
+            }
+            else if (input.StartsWith(Commands.SearchToken))
+            {
+                cmd = Commands.Search;
+                input = input.Substring(Commands.SearchToken.Length);
+            }
+            else
+            {
+                cmd = Commands.Add;
+            }
+
+            return new Tuple<string, string>(cmd, input);
+        }
     }
 }
