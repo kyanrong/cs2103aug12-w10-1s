0acca9b806083375899a859a9fafa8f4fdf5b5d3
Yong Michael <wrong92@gmail.com>
2012-10-16 19:17:33 +0800

    wip.

---
+++ Type/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Type
+{
+    class Commands
+    {
+        public const string Token = ":";
+        public const string Invalid = "invalid";
+        public const string Add = "add";
+        public const string Edit = "edit";
+        public const string Done = "done";
+        public const string Archive = "archive";
+    }
+}
