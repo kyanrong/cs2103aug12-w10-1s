2272328e6c814fe2fa5223099e2906e867a0c006
Kek Yan Rong <kekyanrong@gmail.com>
2012-10-20 11:45:33 +0800

    invalid message popup

---
+++ Type/Task.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+
+namespace Type
+{
+    public class Task
+    {
+
+        private string rawText;
+
+        // Parsed Types
+        public enum ParsedType {
+            String,
+            HashTag,
+            DateTime,
+            Priority
+        }
+        private List<Tuple<string, ParsedType>> tokens;
+        private List<string> tags;
+
+        // Constructor
+        // from row.
+        public Task(List<string> row)
+        {
+            // saves the rawText
+            // parsing should be idempotent
+            // re-parsing on the same rawText
+            // should return the same values
+            this.RawText = row[0];
+            this.Done = Boolean.Parse(row[1]);
+            this.Archive = Boolean.Parse(row[2]);
+            this.Setup();
+        }
+        // from rawText
+        public Task(string rawText)
+        {
+            this.RawText = rawText;
+            this.Done = false;
+            this.Archive = false;
+
+            this.Setup();
+        }
+
+        private void Setup()
+        {
+            // other misc setup
+            // TODO.
+
+            // parse the input
+            this.Parse();
+        }
+
+        private void Parse()
+        {
+            // default token.
+            var result = new List<Tuple<string, ParsedType>>();
+            Tuple<string, ParsedType> t = Tuple.Create(this.RawText, ParsedType.String);
+            result.Add(t);
+            this.tokens = result;
+
+            // parse hashtags
+            this.tags = RegExp.HashTags(this.RawText);
+
+            foreach (string hashtag in this.tags)
+            {
+                // find token contain hashtag.
+                var res = new List<Tuple<string, ParsedType>>();
+                foreach (Tuple<string, ParsedType> token in this.tokens)
+                {
+                    // if not a string. token has been parsed.
+                    if (token.Item2 != ParsedType.String)
+                    {
+                        // add to result.
+                        // no further processing required.
+                        res.Add(token);
+                    }
+                    else
+                    {
+                        if (token.Item1.Contains(hashtag))
+                        {
+                            string[] split = token.Item1.Split(new string[] { hashtag }, StringSplitOptions.None);
+                            res.Add(Tuple.Create(split[0], ParsedType.String));
+                            
+                            res.Add(Tuple.Create(hashtag, ParsedType.HashTag));
+
+                            res.Add(Tuple.Create(split[1], ParsedType.String));
+                        }
+                        else
+                        {
+                            // hashtag not in token.
+                            // add to result.
+                            res.Add(token);
+                        }
+                    }
+                }
+                // replace this.tokens.
+                this.tokens = res;
+            }
+
+            
+        }
+
+        // returns row of strings for storing
+        public List<string> ToRow()
+        {
+            var row = new List<string>();
+            row.Add(this.RawText);
+            row.Add(this.Done.ToString());
+            row.Add(this.Archive.ToString());
+            return row;
+        }
+
+        // Task Done
+        public bool Done { get; set; }
+
+        // Task Archive
+        public bool Archive { get; set; }
+
+        // Other Properties
+        public int Id { get; set; }
+        public DateTime Start { get; private set; }
+        public DateTime End { get; private set; }
+        
+        public string RawText
+        {
+            get
+            {
+                return rawText;
+            }
+            set
+            {
+                rawText = value;
+                this.Parse();
+            }
+        }
+
+        public IList<string> Tags
+        {
+            get { return tags.AsReadOnly(); }
+        }
+        public IList<Tuple<string, ParsedType>> Tokens
+        {
+            get { return tokens.AsReadOnly(); }
+        }
+        public override string ToString()
+        {
+            return this.RawText;
+        }
+    }
+}
