279996258764f7ec2f5a5720935415c30057b92f
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-10-20 14:53:49 +0800

    Implemented Archive All.

---
+++ Type/MainWindowDoCommand.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Type
+{
+    public partial class MainWindow : Window
+    {
+        private void DoAdd(Command result)
+        {
+            //The default command is "add".
+            if (result.Text.Trim() != string.Empty)
+            {
+                ExecuteCommand(result.CommandText, result.Text);
+                inputBox.Clear();
+            }
+        }
+
+        private void DoOther(Command result)
+        {
+            Task selectedTask = null;
+            if (renderedTasks.Count != 0 && result.Text != string.Empty)
+            {
+                selectedTask = renderedTasks[0];
+
+                if (result.CommandText == Command.Edit)
+                {
+                    DoEdit(selectedTask);
+                }
+                else
+                {
+                    inputBox.Clear();
+                }
+            }
+
+            ExecuteCommand(result.CommandText, result.Text, selectedTask);
+            inputBox.Clear();
+        }
+
+        private void DoEdit(Task selectedTask)
+        {
+            //Populate inputBox with edit text.
+            inputBox.Text = selectedTask.RawText;
+            MoveCursorToEndOfWord();
+        }
+
+        private void DoSearch(Command result)
+        {
+            if (result.Text.Trim() != string.Empty)
+            {
+                renderedTasks = GetTasksByHashTag(result.Text.Trim());
+            }
+        }
+    }
+}
