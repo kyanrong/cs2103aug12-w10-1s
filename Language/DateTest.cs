e8784fd2995b204697b0fcdab1e51eae29a18dfe
Yong Michael <wrong92@gmail.com>
2012-10-20 13:58:46 +0800

    fixed white space issue

---
+++ Language/DateTest.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Type;
+
+namespace Language
+{
+    [TestClass]
+    public class DateTest
+    {
+        [TestMethod]
+        public void NegativeTests()
+        {
+            List<string> l = new List<string>();
+            l.Add("This should not match");
+            l.Add("This 12 13 13 should not match");
+            l.Add("This 12112 should not match");
+            l.Add("This January");
+
+            foreach (string x in l)
+            {
+                Assert.AreEqual(RegExp.Date(x).Item1, string.Empty);
+            }
+        }
+
+        [TestMethod]
+        public void dd_m()
+        {
+            string x = "This should 12/1 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "12/1");
+            int year = DateTime.Today.Year;
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(year, 1, 12));
+        }
+
+        [TestMethod]
+        public void dd_mm()
+        {
+            string x = "This should 12/11 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "12/11");
+            int year = DateTime.Today.Year;
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(year, 11, 12));
+        }
+
+        [TestMethod]
+        public void dd_mm_yy()
+        {
+            string x = "This should 12/11/11 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "12/11/11");
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(2011, 11, 12));
+        }
+
+        [TestMethod]
+        public void dd_mm_yyyy()
+        {
+            string x = "This should 12/11/2011 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "12/11/2011");
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(2011, 11, 12));
+        }
+
+        [TestMethod]
+        public void dd_month()
+        {
+            string[] months = {
+               "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+            for (int i = 0; i < months.Length; i++)
+            {
+                string month = months[i];
+                string x = "This should 11 " + month + " match";
+                Assert.AreEqual(RegExp.Date(x).Item1, "11 " + month);
+                int year = DateTime.Today.Year;
+                Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(year, i + 1, 11));
+            }
+            string[] months2 = {
+                "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+            };
+            for (int i = 0; i < months2.Length; i++)
+            {
+                string month = months2[i];
+                string x = "This should 11 " + month + " match";
+                Assert.AreEqual(RegExp.Date(x).Item1, "11 " + month);
+                int year = DateTime.Today.Year;
+                Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(year, i + 1, 11));
+            }
+        }
+
+        [TestMethod]
+        public void dd_month_yyyy()
+        {
+            string x = "This should 12 march 2012 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "12 march 2012");
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(2012, 3, 12));
+        }
+
+        [TestMethod]
+        public void dd_month_yy()
+        {
+            string x = "This should 12 march 12 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "12 march 12");
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(2012, 3, 12));
+        }
+
+        [TestMethod]
+        public void deadline_dd_mm_yy()
+        {
+            string x = "This should by 12/11/11 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "by 12/11/11");
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(2011, 11, 12));
+        }
+
+        [TestMethod]
+        public void deadline_dd_mm_yyyy()
+        {
+            string x = "This should On 12/11/2011 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "On 12/11/2011");
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(2011, 11, 12));
+        }
+
+        [TestMethod]
+        public void deadline_dd_month()
+        {
+            string[] months = {
+               "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+            };
+            for (int i = 0; i < months.Length; i++)
+            {
+                string month = months[i];
+                string x = "This should by 11 " + month + " match";
+                Assert.AreEqual(RegExp.Date(x).Item1, "by 11 " + month);
+                int year = DateTime.Today.Year;
+                Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(year, i + 1, 11));
+            }
+            string[] months2 = {
+                "January", "Febuary", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
+            };
+            for (int i = 0; i < months2.Length; i++)
+            {
+                string month = months2[i];
+                string x = "This should due 11 " + month + " match";
+                Assert.AreEqual(RegExp.Date(x).Item1, "due 11 " + month);
+                int year = DateTime.Today.Year;
+                Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(year, i + 1, 11));
+            }
+        }
+
+        [TestMethod]
+        public void deadline_dd_month_yyyy()
+        {
+            string x = "This should on 12 march 2012 match";
+            Assert.AreEqual(RegExp.Date(x).Item1, "on 12 march 2012");
+            Assert.AreEqual(RegExp.Date(x).Item2, new DateTime(2012, 3, 12));
+        }
+
+        [TestMethod]
+        public void NegativeTests2()
+        {
+            List<string> l = new List<string>();
+            l.Add("Thisby 12 January");
+            l.Add("This overdue 12 January");
+
+            foreach (string x in l)
+            {
+                Assert.AreEqual(RegExp.Date(x).Item1, "12 January");
+            }
+        }
+    }
+}
---
+++ Type/MainWindowKeyUp.xaml.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Type
+{
+    public partial class MainWindow : Window
+    {
+        private void HandleSendCommand()
+        {
+            //Parse input.
+            var result = Command.Parse(inputBox.Text);
+
+            if (result.CommandText == Command.Invalid)
+            {
+                //Alert the user somehow that the command was invalid.
+                //TODO
+                popUp.IsOpen = true;
+                TextBlock invalidMessage = new TextBlock();
+                invalidMessage.Text = TEXT_INVALIDMESSAGE;
+                StyleInvalidMessage(invalidMessage);      
+                popUp.Child = invalidMessage;
+            }
+            else if (result.CommandText == Command.Search)
+            {
+                DoSearch(result);
+            }
+            else if (result.CommandText != Command.Add)
+            {
+                DoOther(result);
+            }
+            else
+            {
+                DoAdd(result);
+            }
+
+            //Retrieve a list of tasks, unless the list has already been retrieved by Search.
+            if (result.CommandText != Command.Search)
+            {
+                renderedTasks = GetTasks(8);
+            }
+
+            RenderTasks();
+        }
+
+        private void HandleAutoComplete()
+        {
+            //AutoComplete is only defined if there are rendered tasks on screen.
+            if (renderedTasks != null && renderedTasks.Count > 0)
+            {
+                var result = Command.Parse(inputBox.Text);
+                if (result.CommandText != Command.Invalid)
+                {
+                    int completeBegin = LCPIndex(result.Text, renderedTasks[0].RawText);
+
+                    if (inputBox.Text.EndsWith(result.CommandText))
+                    {
+                        inputBox.Text += " ";
+                    }
+
+                    if (completeBegin >= 0)
+                    {
+                        inputBox.Text += renderedTasks[0].RawText.Substring(completeBegin + 1);
+                        MoveCursorToEndOfWord();
+                    }
+                }
+            }
+        }
+
+        private void HandleHideWindow()
+        {
+            //If the input box is not empty, we clear the input box, but do not hide the window.
+            //Otherwise, we hide the window.
+            //If the input box contains only whitespace (which will not be caught by the first condition),
+            //we clear it before hiding the window.
+            if (inputBox.Text.Trim() != string.Empty)
+            {
+                inputBox.Clear();
+            }
+            else
+            {
+                if (inputBox.Text.Trim() == string.Empty)
+                {
+                    inputBox.Clear();
+                }
+                this.Hide();
+            }
+        }
+
+        // Used for auto complete.
+        private void MoveCursorToEndOfWord()
+        {
+            inputBox.Select(inputBox.Text.Length, 0);
+        }
+
+        //Finds the longest common prefix of a and b.
+        private int LCPIndex(string a, string b)
+        {
+            int found = -1;
+            for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                if (a[i] == b[i])
+                {
+                    found = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return found;
+        }
+    }
+}
