e155d9e42fb391568327bf16e93ccc6cdbef8dab
Ang Civics <angcivics@gmail.com>
2012-10-24 17:40:47 +0800

    use arrow key to highligh implemented, will do further tidy up later

---
+++ Type/MainWindow.xaml.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Type
+{
+    /// <summary>
+    /// Retrieves a list of suggestions that begin with a specified prefix.
+    /// </summary>
+    /// <param name="partialText">Prefix to match.</param>
+    /// <returns>Read-only list of suggestions as strings.</returns>
+    public delegate IList<Task> FilterSuggestionsCallback(string partialText);
+
+    /// <summary>
+    /// Parses a raw string and executes its command, if valid.
+    /// If no valid command is found, this method does nothing.
+    /// </summary>
+    /// <param name="cmd">Command.</param>
+    /// <param name="content">Text of the Command.</param>
+    /// <param name="selected">Selected task. Throws an exception if no reference is specified, but the command requires one.</param>
+    public delegate void ExecuteCommandCallback(string cmd, string content, Task selectedTask = null);
+
+    /// <summary>
+    /// Retrieves a list of tasks to be displayed.
+    /// </summary>
+    /// <param name="num">Number of tasks to retrieve.</param>
+    /// <returns>Read-only list of tasks.</returns>
+    public delegate IList<Task> GetTasksCallback(int num);
+
+    /// <summary>
+    /// Retrieves a list of tasks tagged with at least one hash tag.
+    /// </summary>
+    /// <param name="content">String containing hash tags separated by ' '.</param>
+    /// <returns>Read-only list of tasks.</returns>
+    public delegate IList<Task> GetTasksByHashTagCallback(string content);
+
+    public partial class MainWindow : Window
+    {
+        private const string TEXT_WELCOME = "start typing...";
+        private const string TEXT_NOTASKS = "no tasks.";
+
+        private List<String> helpDescription = new List<String>();
+        private List<String> helpCommands = new List<String>();
+
+        private ExecuteCommandCallback ExecuteCommand;
+        private FilterSuggestionsCallback GetFilterSuggestions;
+        private GetTasksCallback GetTasks;
+        private GetTasksByHashTagCallback GetTasksByHashTag;
+
+        private IList<Task> renderedTasks;
+
+        private int highlightIndex;
+
+        public MainWindow(FilterSuggestionsCallback GetFilterSuggestions, ExecuteCommandCallback ExecuteCommand, GetTasksCallback GetTasks, GetTasksByHashTagCallback GetTasksByHashTag)
+        {
+            InitializeComponent();
+
+            this.GetFilterSuggestions = GetFilterSuggestions;
+            this.ExecuteCommand = ExecuteCommand;
+            this.GetTasks = GetTasks;
+            this.GetTasksByHashTag = GetTasksByHashTag;
+
+            // focus cursor in input box
+            inputBox.Focus();
+
+            // display input label
+            DisplayInputLabel();
+
+            // populate help lists
+            PopulateHelpLists();
+
+            // bootstrap tasks
+            // TODO. abstract this number.
+            renderedTasks = GetTasks(6);
+            RenderTasks();
+
+            highlightIndex = 0;
+        }
+
+        // Input Label
+        private void DisplayInputLabel()
+        {
+            if (inputBox.Text.Trim() == "")
+            {
+                inputBoxLabel.Content = TEXT_WELCOME;
+            }
+            else
+            {
+                inputBoxLabel.Content = "";
+            }
+        }
+
+        // Render List of Tasks
+        private void RenderTasks(Boolean filter = false)
+        {
+            tasksGrid.Children.Clear();
+            if (renderedTasks.Count == 0)
+            {
+                // display no tasks text.
+                StackPanel noTasksText = new StackPanel();
+
+                TextBlock text = new TextBlock();
+                text.Text = TEXT_NOTASKS;
+
+                StyleNoTasks(text);
+
+                noTasksText.Children.Add(text);
+
+                // Append to tasksgrid.
+                tasksGrid.Children.Add(noTasksText);
+
+                DisplayBlueBorder(noTasksText);
+            }
+            else
+            {
+                // loop over each task and create task view
+                // append each to tasks grid
+                for (int j = 0 ; j<renderedTasks.Count ; j++)
+                {
+                    // create single stacked panel w/ info
+                    StackPanel taskView = new StackPanel();
+                    TextBlock text = new TextBlock();
+                   
+                    // style tokens within the textblock
+                    for (int i = 0; i< renderedTasks[j].Tokens.Count; i++)
+                    {
+                        Tuple<string, Task.ParsedType> tuple = renderedTasks[j].Tokens[i];
+
+                        Run run = new Run(tuple.Item1);
+                        // Style Runs
+                        if (renderedTasks[j].Done)
+                        {
+                            StyleDoneParsedTypes(run);
+                        }
+                        else
+                        {
+                            TextChange(tuple, run);
+                        }
+
+                        // if rendering filters
+
+                        if (j == highlightIndex)
+                        {
+                            // TODO.
+                            // put styles here for highlighted first task
+                            text.Background = Brushes.Beige;
+
+                        }
+
+                        text.Inlines.Add(run);
+                    }
+
+                    // style accordingly
+                    if (renderedTasks[j].Done)
+                    {
+                        StyleDone(text);
+                    }
+
+                    StyleTasks(text);
+
+                    taskView.Children.Add(text);
+
+                    // append task view to grid view
+                    tasksGrid.Children.Add(taskView);
+
+                    DisplayBlueBorder(taskView);
+                }
+            }
+            
+            DisplayDashedBorder(tasksGrid);
+        }
+
+        private void TextChange(Tuple<string, Task.ParsedType> tuple, Run run)
+        {
+            if (tuple.Item2 == Task.ParsedType.HashTag)
+            {
+                StyleHashTags(run);
+            }
+
+            // Style Dates
+            if (tuple.Item2 == Task.ParsedType.DateTime)
+            {
+                StyleDateTime(run);
+            }
+
+            // Style PriorityHigh
+            if (tuple.Item2 == Task.ParsedType.PriorityHigh)
+            {
+                StylePriorityHigh(run);
+            }
+
+            // Style PriorityLow
+            if (tuple.Item2 == Task.ParsedType.PriorityLow)
+            {
+                StylePriorityLow(run);
+            }
+        }
+
+        // Event Listener when Input Box text changes.
+        private void InputBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            DisplayInputLabel();
+            bool filter = false;
+            if (inputBox.Text == string.Empty)
+            {
+                renderedTasks = GetTasks(8);
+            }
+            else
+            {
+                var result = Command.Parse(inputBox.Text);
+                if (result.CommandText == Command.Search)
+                {
+                    renderedTasks = GetTasksByHashTag(result.Text);
+                    filter = true;
+                }
+                else if (result.CommandText != Command.Add)
+                {
+                    renderedTasks = GetFilterSuggestions(result.Text);
+                    filter = true;
+                }
+            }
+
+            RenderTasks(filter);
+            invalidCmdPopup.IsOpen = false;
+            helpDescriptionPopup.IsOpen = false;
+            helpCommandsPopup.IsOpen = false;
+        }
+
+        // Event Listener, onKeyUp Input Box
+        private void InputBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    HandleSendCommand();
+                    break;
+
+                case Key.Tab:
+                    HandleAutoComplete();
+                    break;
+
+                case Key.Escape:
+                    HandleHideWindow();
+                    break;
+
+                case Key.Up:
+                    highlightIndex--;
+                    RenderTasks();
+                    break;
+                    
+                case Key.Down:
+                    highlightIndex++;
+                    RenderTasks();
+                    break;
+
+            }
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void PopulateHelpLists()
+        {
+            // Populate helpDescription List
+            helpDescription.Add("Create new task");
+            helpDescription.Add("Complete a task");
+            helpDescription.Add("Complete all tasks with a tag");
+            helpDescription.Add("Archive all completed tasks");
+            helpDescription.Add("Archive a single task");
+            helpDescription.Add("Archive all tasks with a tag");
+            helpDescription.Add("Edit a task");
+            helpDescription.Add("Undo last action");
+            helpDescription.Add("Filter by hash tags");
+            helpDescription.Add("Show archived tasks");
+            helpDescription.Add("Sort the display");
+
+            // Populate helpCommands List
+            helpCommands.Add("<task>");
+            helpCommands.Add(":done <task>");
+            helpCommands.Add(":done #<tag name>");
+            helpCommands.Add(":archive");
+            helpCommands.Add(":archive <task>");
+            helpCommands.Add(":archive #<tag name>");
+            helpCommands.Add(":edit <task>");
+            helpCommands.Add(":undo");
+            helpCommands.Add("/<tag name> [<tag name>] ...");
+            helpCommands.Add("/archive <tag name> [<tag name>] ...");
+            helpCommands.Add(":sort <field>");
+        }
+
+        
+    }
+}
