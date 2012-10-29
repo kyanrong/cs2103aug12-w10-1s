4d092b8040a917bd5c37edb5448898beb8545222
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-10-29 08:47:42 +0800

    Refcatored. Remove broken commits.

---
+++ Type/MainWindow.xaml.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Type
+{
+    /// <summary>
+    /// Retrieves a list of suggestions that begin with a specified prefix.
+    /// </summary>
+    /// <param name="partialText">Prefix to match.</param>
+    /// <returns>Read-only list of suggestions as strings.</returns>
+    public delegate IList<Task> FilterSuggestionsCallback(string partialText);
+
+    /// <summary>
+    /// Parses a raw string and executes its command, if valid.
+    /// If no valid command is found, this method does nothing.
+    /// </summary>
+    /// <param name="cmd">Command.</param>
+    /// <param name="content">Text of the Command.</param>
+    /// <param name="selected">Selected task. Throws an exception if no reference is specified, but the command requires one.</param>
+    public delegate void ExecuteCommandCallback(string cmd, string content, Task selectedTask = null);
+
+    /// <summary>
+    /// Retrieves a list of tasks to be displayed.
+    /// </summary>
+    /// <param name="num">Number of tasks to retrieve.</param>
+    /// <returns>Read-only list of tasks.</returns>
+    public delegate IList<Task> GetTasksCallback(int num);
+
+    /// <summary>
+    /// Retrieves a list of tasks tagged with at least one hash tag.
+    /// </summary>
+    /// <param name="content">String containing hash tags separated by ' '.</param>
+    /// <returns>Read-only list of tasks.</returns>
+    public delegate IList<Task> GetTasksByHashTagCallback(string content);
+
+    public partial class MainWindow : Window
+    {
+        private const string TEXT_WELCOME = "start typing...";
+        private const string TEXT_NOTASKS = "no tasks.";
+        private const int NUMBER_OF_TASKS_LOADED = 50;
+        private const int NUMBER_OF_TASKS_DISPLAYED = 6;
+
+        private List<String> helpDescription = new List<String>();
+
+        private ExecuteCommandCallback ExecuteCommand;
+        private FilterSuggestionsCallback GetFilterSuggestions;
+        private GetTasksCallback GetTasks;
+        private GetTasksByHashTagCallback GetTasksByHashTag;
+
+        private IList<Task> renderedTasks;
+        private List<TextBlock> taskTextBlockList;
+        private int highlightIndex, listStartIndex, listEndIndex;
+        private StackPanel taskView = new StackPanel();
+
+        public MainWindow(FilterSuggestionsCallback GetFilterSuggestions, ExecuteCommandCallback ExecuteCommand, GetTasksCallback GetTasks, GetTasksByHashTagCallback GetTasksByHashTag)
+        {
+            InitializeComponent();
+
+            this.GetFilterSuggestions = GetFilterSuggestions;
+            this.ExecuteCommand = ExecuteCommand;
+            this.GetTasks = GetTasks;
+            this.GetTasksByHashTag = GetTasksByHashTag;
+
+            // focus cursor in input box
+            inputBox.Focus();
+
+            // display input label
+            DisplayInputLabel();
+
+            // populate help lists
+            PopulateHelpList();
+
+            // bootstrap tasks
+            // TODO. abstract this number.
+            renderedTasks = GetTasks(NUMBER_OF_TASKS_LOADED);
+            taskTextBlockList = new List<TextBlock>();
+            InitializeListBounderIndex();
+            highlightIndex = 0;
+            RenderTasks();
+        }
+
+        // Input Label
+        private void DisplayInputLabel()
+        {
+            if (inputBox.Text.Trim() == "")
+            {
+                inputBoxLabel.Content = TEXT_WELCOME;
+            }
+            else
+            {
+                inputBoxLabel.Content = "";
+            }
+        }
+
+        // Render List of Tasks
+        private void RefreshViewList()
+        {
+            taskView.Children.Clear();
+            tasksGrid.Children.Clear();
+            TextBlock text = new TextBlock();
+
+            if (renderedTasks.Count == 0)
+            {
+                text.Text = TEXT_NOTASKS;
+
+                StyleNoTasks(text);
+
+                taskView.Children.Add(text);
+                DisplayBlueBorder(taskView);
+            }
+            else
+            {
+                DisplayBlueBorder(taskView);
+                // loop over each task and create task view
+                // append each to tasks grid
+                for (int i = listStartIndex; i < listEndIndex; i++)
+                {  
+                    text = taskTextBlockList[i];
+                    //highlight target textbox                    
+
+                    if (i == highlightIndex + listStartIndex)
+                    {
+                        text.Background = Brushes.Beige;
+                    }
+                    else
+                    {
+                        text.Background = Brushes.White;
+                    }
+                    taskView.Children.Add(text);
+                    DisplayBlueBorder(taskView);
+                }
+            }
+
+            // append task view to grid view
+            tasksGrid.Children.Add(taskView);
+            DisplayDashedBorder(tasksGrid);
+        }
+
+        //generate list of text block
+        private void RenderTasks()
+        {
+            taskTextBlockList.Clear();
+            InitializeListBounderIndex();
+
+            for (int i = 0; i < renderedTasks.Count; i++)
+            {
+                TextBlock text = new TextBlock();
+                // style tokens within the textblock
+                foreach (Tuple<string, Task.ParsedType> tuple in renderedTasks[i].Tokens)
+                {
+                    Run run = new Run(tuple.Item1);
+                    // Style Runs
+                    if (renderedTasks[i].Done)
+                    {
+                        StyleDoneParsedTypes(run);
+                    }
+                    else
+                    {
+                        if (tuple.Item2 == Task.ParsedType.HashTag)
+                        {
+                            StyleHashTags(run);
+                        }
+
+                        // Style Dates
+                        if (tuple.Item2 == Task.ParsedType.DateTime)
+                        {
+                            StyleDateTime(run);
+                        }
+
+                        // Style PriorityHigh
+                        if (tuple.Item2 == Task.ParsedType.PriorityHigh)
+                        {
+                            StylePriorityHigh(run);
+                        }
+
+                        // Style PriorityLow
+                        if (tuple.Item2 == Task.ParsedType.PriorityLow)
+                        {
+                            StylePriorityLow(run);
+                        }
+                    }
+                    text.Inlines.Add(run);
+                }
+                StyleTasks(text);
+                taskTextBlockList.Add(text);
+            }
+            RefreshViewList();
+        }
+
+        private void InitializeListBounderIndex()
+        {
+            highlightIndex = 0;
+            listStartIndex = 0;
+
+            if (renderedTasks.Count > 6)
+            {
+                listEndIndex = 6;
+            }
+            else
+            {
+                listEndIndex = renderedTasks.Count;
+            }            
+        }
+
+        // Event Listener when Input Box text changes.
+        private void InputBoxTextChanged(object sender, TextChangedEventArgs e)
+        {
+            DisplayInputLabel();
+
+            if (inputBox.Text == string.Empty)
+            {
+                renderedTasks = GetTasks(NUMBER_OF_TASKS_LOADED);
+            }
+            else
+            {
+                var result = Command.Parse(inputBox.Text);
+                if (result.CommandText == Command.Search)
+                {
+                    renderedTasks = GetTasksByHashTag(result.Text);
+                    InitializeListBounderIndex();
+                }
+                else if (result.CommandText != Command.Add)
+                {
+                    renderedTasks = GetFilterSuggestions(result.Text);
+                    InitializeListBounderIndex();
+                }
+            }
+            RenderTasks();
+            invalidCmdPopup.IsOpen = false;
+            helpDescriptionPopup.IsOpen = false;
+        }
+
+        // Event Listener, onKeyUp Input Box
+        private void InputBoxKeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    HandleSendCommand();
+                    renderedTasks = GetTasks(NUMBER_OF_TASKS_LOADED);
+                    RenderTasks();
+                    break;
+
+                case Key.Tab:
+                    HandleAutoComplete();
+                    break;
+
+                case Key.Escape:
+                    HandleHideWindow();
+                    break;
+
+                case Key.Up:
+                    HandleUpArrow();
+                    break;
+
+                case Key.Down:
+                    HandleDownArrow();
+                    break;
+
+                case Key.Left:
+                    HandleLeftArrow();
+                    break;
+
+                case Key.Right:
+                    HandleRightArrow();
+                    break;
+            }
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void PopulateHelpList()
+        {
+            // Populate helpDescription List
+            helpDescription.Add("Create new task");
+            helpDescription.Add("<task>");
+
+            helpDescription.Add("Complete a task");
+            helpDescription.Add(":done <task>");
+
+            helpDescription.Add("Complete all tasks with a tag");
+            helpDescription.Add(":done #<tag name>");
+
+            helpDescription.Add("Archive all completed tasks");
+            helpDescription.Add(":archive");
+
+            helpDescription.Add("Archive a single task");
+            helpDescription.Add(":archive <task>");
+
+            helpDescription.Add("Archive all tasks with a tag");
+            helpDescription.Add(":archive #<tag name>");
+
+            helpDescription.Add("Edit a task");
+            helpDescription.Add(":edit <task>");
+
+            helpDescription.Add("Undo last action");
+            helpDescription.Add(":undo");
+
+            helpDescription.Add("Filter by hash tags");
+            helpDescription.Add("/<tag name> [<tag name>] ...");
+
+            helpDescription.Add("Show archived tasks");
+            helpDescription.Add("/archive <tag name> [<tag name>] ...");
+
+            helpDescription.Add("Sort the display");
+            helpDescription.Add(":sort <field>");
+        }
+    }
+}
---
+++ Type/MainWindowKeyUp.xaml.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Type
+{
+    public partial class MainWindow : Window
+    {
+        private void HandleSendCommand()
+        {
+            //Parse input.
+            var result = Command.Parse(inputBox.Text);
+
+            switch (result.CommandText)
+            {
+                case Command.Invalid:
+                    invalidCmdPopup.IsOpen = true;
+                    break;
+
+                case Command.Search:
+                    DoSearch(result);
+                    break;
+
+                case Command.Add:
+                    DoAdd(result);
+                    break;
+
+                case Command.Help:
+                    DoHelp();
+                    break;
+
+                case Command.Archive:
+                case Command.Done:
+                case Command.Undo:
+                case Command.Edit:
+                    DoOther(result);
+                    break;
+                    
+                default:
+                    break;
+            }
+
+            //Retrieve a list of tasks, unless the list has already been retrieved by Search.
+            if (result.CommandText != Command.Search)
+            {
+                renderedTasks = GetTasks(NUMBER_OF_TASKS_LOADED);
+            }
+
+            RenderTasks();
+        }
+
+        private void HandleAutoComplete()
+        {
+            //AutoComplete is only defined if there are rendered tasks on screen.
+            if (renderedTasks != null && renderedTasks.Count > 0)
+            {
+                var result = Command.Parse(inputBox.Text);
+                if (result.CommandText != Command.Invalid)
+                {
+                    int completeBegin = LCPIndex(result.Text, renderedTasks[0].RawText);
+
+                    if (inputBox.Text.EndsWith(result.CommandText))
+                    {
+                        inputBox.Text += " ";
+                    }
+
+                    if (completeBegin >= 0)
+                    {
+                        inputBox.Text += renderedTasks[0].RawText.Substring(completeBegin + 1);
+                        MoveCursorToEndOfWord();
+                    }
+                }
+                else
+                {
+                    inputBox.Text += Command.TryComplete(inputBox.Text.Substring(1));
+                    MoveCursorToEndOfWord();
+                }
+            }
+        }
+
+        private void HandleHideWindow()
+        {
+            //If the input box is not empty, we clear the input box, but do not hide the window.
+            //Otherwise, we hide the window.
+            //If the input box contains only whitespace (which will not be caught by the first condition),
+            //we clear it before hiding the window.
+            if (inputBox.Text.Trim() != string.Empty)
+            {
+                inputBox.Clear();
+            }
+            else
+            {
+                if (inputBox.Text.Trim() == string.Empty)
+                {
+                    inputBox.Clear();
+                }
+                this.Hide();
+            }
+        }
+
+        private void HandleUpArrow()
+        {
+            highlightIndex--;
+            if (highlightIndex < 0 && listStartIndex > 0)
+            {
+                highlightIndex = NUMBER_OF_TASKS_DISPLAYED-1;
+                listStartIndex -= NUMBER_OF_TASKS_DISPLAYED ;
+                if (listStartIndex < 0)
+                {
+                    listStartIndex = 0;
+                    highlightIndex = listStartIndex;                 
+                }
+                listEndIndex = listStartIndex + NUMBER_OF_TASKS_DISPLAYED;
+            }
+            if (highlightIndex < 0)
+            {
+                highlightIndex = 0;
+            }
+            RefreshViewList();
+        }
+        private void HandleDownArrow()
+        {
+            highlightIndex++;
+            if ((highlightIndex > NUMBER_OF_TASKS_DISPLAYED-1) && (listEndIndex != renderedTasks.Count))
+            {
+                listStartIndex += NUMBER_OF_TASKS_DISPLAYED;
+                listEndIndex = listStartIndex + NUMBER_OF_TASKS_DISPLAYED;
+                highlightIndex = 0;
+                if (listEndIndex >= renderedTasks.Count)
+                {
+                    highlightIndex += (listEndIndex - renderedTasks.Count);
+                    listEndIndex = renderedTasks.Count;
+                }
+                listStartIndex = listEndIndex - NUMBER_OF_TASKS_DISPLAYED;
+            }
+            if (highlightIndex > NUMBER_OF_TASKS_DISPLAYED-1)
+            {
+                highlightIndex = NUMBER_OF_TASKS_DISPLAYED-1;
+            }
+            RefreshViewList();
+        }
+        //go to previous page
+        private void HandleLeftArrow()
+        {
+            //already at the first page, no need changes
+            if (listStartIndex == 0)
+            {
+                return;
+            }
+            listStartIndex -= NUMBER_OF_TASKS_DISPLAYED;            
+            listEndIndex = listStartIndex + NUMBER_OF_TASKS_DISPLAYED;
+            CheckListIndexBound();
+            RefreshViewList();
+        }
+        //go to next page
+        private void HandleRightArrow()
+        {
+            //already at the last page, no need changes
+            if (listEndIndex == renderedTasks.Count)
+            {
+                return;
+            }
+            listEndIndex += NUMBER_OF_TASKS_DISPLAYED;
+            listStartIndex = listEndIndex - NUMBER_OF_TASKS_DISPLAYED;
+
+            if (CheckListIndexBound() && (highlightIndex > listEndIndex % NUMBER_OF_TASKS_DISPLAYED - 1))
+            {
+                highlightIndex = listEndIndex % NUMBER_OF_TASKS_DISPLAYED - 1;
+            }
+            RefreshViewList();
+        }
+        //if the list index is out of bound then set to the correct bound
+        private bool CheckListIndexBound()
+        {
+            bool isChanged = false;
+            if (listStartIndex < 0)
+            {
+                listStartIndex = 0;
+                isChanged = true;
+            }
+            if (listEndIndex > renderedTasks.Count)
+            {
+                listEndIndex = renderedTasks.Count;
+                isChanged = true;
+            }
+            return isChanged;
+        }
+        
+        // Used for auto complete.
+        private void MoveCursorToEndOfWord()
+        {
+            inputBox.Select(inputBox.Text.Length, 0);
+        }
+
+        //Finds the longest common prefix of a and b.
+        private int LCPIndex(string a, string b)
+        {
+            int found = -1;
+            for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                if (a[i] == b[i])
+                {
+                    found = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return found;
+        }
+    }
+}
