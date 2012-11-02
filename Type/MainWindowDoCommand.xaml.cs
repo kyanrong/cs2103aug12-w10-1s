c0bb33186e5e7f456a04362a16b2c5b12a8a5821
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-11-02 13:18:32 +0800

    Rollback.

---
+++ Type/MainWindowDoCommand.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+
+namespace Type
+{
+    public partial class MainWindow : Window
+    {
+        private void DoAdd(Command result)
+        {
+            //The default command is "add".
+            if (result.Text.Trim() != string.Empty)
+            {
+                ExecuteCommand(result.CommandText, result.Text);
+                inputBox.Clear();
+            }
+        }
+
+        private void DoOther(Command result)
+        {
+            Task target = null;
+
+            if (renderedTasks.Count > 0 && result.Text != string.Empty)
+            {
+                target = selectedTask;
+
+                if (result.CommandText == Command.Edit)
+                {
+                    DoEdit(target);
+                }
+            }
+
+            ExecuteCommand(result.CommandText, result.Text, target);
+
+            if (result.CommandText != Command.Edit)
+            {
+                inputBox.Clear();
+            }
+        }
+
+        private void DoEdit(Task selectedTask)
+        {
+            //Populate inputBox with edit text.
+            inputBox.Text = selectedTask.RawText;
+            MoveCursorToEndOfWord();
+        }
+
+        private void DoSearch(Command result)
+        {
+            if (result.Text.Trim() != string.Empty)
+            {
+                renderedTasks = GetTasksByHashTag(result.Text.Trim());
+            }
+        }
+
+        private void DoHelp()
+        {
+            helpDescriptionListBox.DataContext = helpDescription;
+
+            helpDescriptionPopup.IsOpen = true;
+        }
+    }
+}
---
+++ Type/MainWindowKeyUp.xaml.cs
@@ -0,0 +1,227 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Type
+{
+    public partial class MainWindow : Window
+    {
+        private void HandleSendCommand()
+        {
+            //Parse input.
+            var result = Command.Parse(inputBox.Text);
+
+            switch (result.CommandText)
+            {
+                case Command.Invalid:
+                    invalidCmdPopup.IsOpen = true;
+                    break;
+
+                case Command.Search:
+                    DoSearch(result);
+                    break;
+
+                case Command.Add:
+                    DoAdd(result);
+                    break;
+
+                case Command.Help:
+                    DoHelp();
+                    break;
+
+                case Command.Archive:
+                case Command.Done:
+                case Command.Undo:
+                case Command.Edit:
+                case Command.Clear:
+                    DoOther(result);
+                    break;
+                    
+                default:
+                    break;
+            }
+
+            //Retrieve a list of tasks, unless the list has already been retrieved by Search.
+            if (result.CommandText != Command.Search)
+            {
+                renderedTasks = GetTasks(NUMBER_OF_TASKS_LOADED);
+            }
+
+            RenderTasks();
+        }
+
+        private void HandleAutoComplete()
+        {
+            //AutoComplete is only defined if there are rendered tasks on screen.
+            if (renderedTasks != null && renderedTasks.Count > 0)
+            {
+                var result = Command.Parse(inputBox.Text);
+                if (result.CommandText != Command.Invalid)
+                {
+                    int completeBegin = LCPIndex(result.Text, renderedTasks[0].RawText);
+
+                    if (inputBox.Text.EndsWith(result.CommandText))
+                    {
+                        inputBox.Text += " ";
+                    }
+
+                    if (completeBegin >= 0)
+                    {
+                        inputBox.Text += renderedTasks[0].RawText.Substring(completeBegin + 1);
+                        MoveCursorToEndOfWord();
+                    }
+                }
+                else
+                {
+                    inputBox.Text += Command.TryComplete(inputBox.Text.Substring(1));
+                    MoveCursorToEndOfWord();
+                }
+            }
+        }
+
+        private void HandleHideWindow()
+        {
+            //If the input box is not empty, we clear the input box, but do not hide the window.
+            //Otherwise, we hide the window.
+            //If the input box contains only whitespace (which will not be caught by the first condition),
+            //we clear it before hiding the window.
+            if (inputBox.Text.Trim() != string.Empty)
+            {
+                inputBox.Clear();
+            }
+            else
+            {
+                if (inputBox.Text.Trim() == string.Empty)
+                {
+                    inputBox.Clear();
+                }
+                this.Hide();
+            }
+        }
+
+        //modify the highlight index and may go to the previous page.
+        private void HandleUpArrow()
+        {
+            highlightIndex--;
+
+            //when highlighIndex out of bound and current page is not the first page
+            if (highlightIndex < 0 && listStartIndex > 0)
+            {
+                HandleLeftArrow();//move to previous page
+                highlightIndex = (listEndIndex-1) % NUMBER_OF_TASKS_DISPLAYED;
+            }
+
+            CheckHighlightIndexBound();
+
+            RefreshViewList();
+        }
+
+        //modify the highlightIndex and may go to next page
+        private void HandleDownArrow()
+        {
+            highlightIndex++;
+
+            if ((highlightIndex > NUMBER_OF_TASKS_DISPLAYED-1) && (listEndIndex != renderedTasks.Count))
+            {
+                HandleRightArrow();//move to next page
+                highlightIndex = 0;
+            }
+
+            CheckHighlightIndexBound();
+
+            RefreshViewList();
+        }
+
+        //go to previous page, will modify listStartIndex and listEndIndex
+        //may modify highlightIndex
+        private void HandleLeftArrow()
+        {
+            //already at the first page, no need changes
+            if (listStartIndex == 0)
+            {
+                return;
+            }
+
+            listStartIndex -= NUMBER_OF_TASKS_DISPLAYED;            
+            listEndIndex = listStartIndex + NUMBER_OF_TASKS_DISPLAYED;
+
+            CheckListIndexBound();
+            CheckHighlightIndexBound();
+            RefreshViewList();
+        }
+
+        //go to next page, will modify listStartIndex and listEndIndex
+        //may modify highlightIndex
+        private void HandleRightArrow()
+        {
+            //already at the last page, no need changes
+            if (listEndIndex == renderedTasks.Count)
+            {
+                return;
+            }
+
+            listEndIndex += NUMBER_OF_TASKS_DISPLAYED;
+            listStartIndex = listEndIndex - NUMBER_OF_TASKS_DISPLAYED;
+
+            CheckListIndexBound();
+            CheckHighlightIndexBound();
+            RefreshViewList();
+        }
+
+        //if the list index is out of bound then set it back to the correct bound
+        private void CheckListIndexBound()
+        {
+            if (listStartIndex < 0)
+            {
+                listStartIndex = 0;
+            }
+
+            if (listEndIndex > renderedTasks.Count)
+            {
+                listEndIndex = renderedTasks.Count;
+            }
+        }
+
+        //if the highLightIndex out of bound the set it back to the correct bound
+        private void CheckHighlightIndexBound()
+        {
+            if (highlightIndex < 0)
+            {
+                highlightIndex = 0;
+            }
+            if (highlightIndex > (listEndIndex-1) % NUMBER_OF_TASKS_DISPLAYED)
+            {
+                highlightIndex = (listEndIndex-1) % NUMBER_OF_TASKS_DISPLAYED;
+            }
+        }
+        
+        // Used for auto complete.
+        private void MoveCursorToEndOfWord()
+        {
+            inputBox.Select(inputBox.Text.Length, 0);
+        }
+
+        //Finds the longest common prefix of a and b.
+        private int LCPIndex(string a, string b)
+        {
+            int found = -1;
+            for (int i = 0; i < Math.Min(a.Length, b.Length); i++)
+            {
+                if (a[i] == b[i])
+                {
+                    found = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return found;
+        }
+    }
+}
