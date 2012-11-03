85dcdb6886a7b2468217fd367aa73f0c6a216ee7
Yong Michael <wrong92@gmail.com>
2012-11-03 16:55:31 +0800

    v stupid bug. regex level up.

---
+++ Type/Command.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics;
+
+namespace Type
+{
+    sealed class Command
+    {
+        #region Fields
+        private static HashSet<string> acceptedCommands;
+
+        public string CommandText { get; set; }
+        public string Text { get; set; }
+        #endregion
+
+        #region Methods
+        // @author A0092104
+        public Command(string CommandText, string Text)
+        {
+            Debug.Assert(!CommandText.StartsWith(Command.Token));
+
+            this.CommandText = CommandText;
+            this.Text = Text;
+        }
+        #endregion
+
+        #region Static Methods
+        // Initialize acceptedCommands map.
+        static Command()
+        {
+            acceptedCommands = new HashSet<string>();
+            acceptedCommands.Add(Command.Archive);
+            acceptedCommands.Add(Command.Done);
+            acceptedCommands.Add(Command.Edit);
+            acceptedCommands.Add(Command.Undo);
+            acceptedCommands.Add(Command.Help);
+            acceptedCommands.Add(Command.Clear);
+
+            // Ensure the hash table contains exactly 6 commands.
+            Debug.Assert(acceptedCommands.Count == 6);
+        }
+
+        // @author A0092104
+        /// <summary>
+        /// Tries to auto complete a command based on the supplied prefix.
+        /// </summary>
+        /// <param name="partial">Prefix to match.</param>
+        /// <returns>Full text of matched command, excluding token, if matched. Otherwise, empty string.</returns>
+        public static string TryComplete(string partial)
+        {
+            Debug.Assert(partial != null);
+
+            // The completion of the empty string is invalid.
+            if (partial == string.Empty)
+            {
+                return partial;
+            }
+
+            var result = acceptedCommands.FirstOrDefault(cmdText => cmdText.StartsWith(partial));
+
+            // If there are no results, we return an empty completion.
+            if (result == null)
+            {
+                return string.Empty;
+            }
+            else
+            {
+                return result.Substring(partial.Length);
+            }
+        }
+
+        // @author A0092104
+        /// <summary>
+        /// Parses input by splitting it into a token containing the command's text, and a token containing the rest of the input.
+        /// </summary>
+        /// <param name="input">Input to parse. Commands should start with the symbol defined in COMMAND_TOKEN.</param>
+        /// <returns>A Command object containing the command text and remaining input.</returns>
+        public static Command Parse(string input)
+        {
+            Debug.Assert(input != null);
+
+            string cmd;
+            // Handle Type's command prefix.
+            if (input.StartsWith(Command.Token))
+            {
+                input = RemoveCommandToken(input);
+
+                // Split given command into a pair of <Command, Input>
+                cmd = SplitCommand(ref input);
+              
+                // First check if invalid.
+                if (!acceptedCommands.Contains(cmd))
+                {
+                    cmd = Command.Invalid;
+                    input = string.Empty;
+                }
+                // If valid, handle special cases:
+                else if (cmd == Command.Help)
+                {
+                    input = string.Empty;
+                }
+            }
+            // Handle special command prefixes.
+            else if (input.StartsWith(Command.SearchToken))
+            {
+                cmd = Command.Search;
+                input = input.Substring(Command.SearchToken.Length);
+            }
+            else if (input.StartsWith(Command.HelpToken))
+            {
+                cmd = Command.Help;
+                input = string.Empty;
+            }
+            // The default command is 'Add'.
+            else
+            {
+                cmd = Command.Add;
+            }
+
+            return new Command(cmd, input.Trim());
+        }
+
+        //@author A0092104
+        private static string SplitCommand(ref string input)
+        {
+            string cmd;
+            int spIndex = input.IndexOf(' ');
+            if (spIndex > 0)
+            {
+                cmd = input.Substring(0, spIndex);
+                input = input.Substring(spIndex + 1);
+            }
+            else
+            {
+                cmd = input.Trim();
+                input = string.Empty;
+            }
+            return cmd;
+        }
+
+        // @author A0092104
+        private static string RemoveCommandToken(string input)
+        {
+            input = input.Substring(Command.Token.Length);
+            return input;
+        }
+        #endregion
+
+        #region Static Enumerations
+        //Tokens.
+        public const string Token = ":";
+        public const string SearchToken = "/";
+
+        //Enumeration of accepted explicit commands.
+        public const string Edit = "edit";
+        public const string Done = "done";
+        public const string Undo = "undo";
+        public const string Archive = "archive";
+        public const string HelpToken = "?";
+        public const string Help = "help";
+        public const string Clear = "clear";
+
+        //Enumeration of implicit commands.
+        public const string Add = "add";
+        public const string Invalid = "invalid";
+        public const string Search = "search";
+        #endregion
+    }
+}
---
+++ Type/RegExp.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Type
+{
+    public class RegExp
+    {
+        // date re
+        // 1. DDMM[YY[YY]]
+        public static string DATE1 = "\\d{1,2}\\/\\d{1,2}(?:\\/\\d{2,4})?";
+        // 2 DD string_rep_of_month [YY[YY]]
+        public static string DATE2 = "\\d{1,2}\\s(?:january|febuary|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)(?:\\s\\d{2,4})?";
+        
+        // time re
+        // 1. 2pm, 2am, 12am/pm
+        public static string TIME1 = "\\b\\d{1,2}(?:am|pm)\\b";
+        // 2. 12:00, 13:00am/pm, 9:00
+        public static string TIME2 = "\b\\d{1,2}:\\d{2}(?:am|pm)?\\b";
+
+        // Combine cases.
+        public static string DateRE = DATE1 + "|" + DATE2;
+        public static string TimeRE = TIME1 + "|" + TIME2;
+
+        // datetime re
+        // 1. date [time]
+        public static string DateTime1 = DateRE + "(?:\\s" + TimeRE + ")?";
+        // 2. time [date]
+        public static string DateTime2 = TimeRE + "(?:\\s" + DateRE + ")?";
+
+        // Combine cases
+        public static string DateTimeRE = DateTime1 + "|" + DateTime2;
+
+
+        public static Tuple<string, int> Priority(string input)
+        {
+            Match m;
+            Regex plus = new Regex("\\B\\+(\\d+)$");
+            m = plus.Match(input);
+            if (m.Success)
+            {
+                return Tuple.Create(m.Value, int.Parse(m.Groups[1].Value));
+            }
+            
+            Regex minus = new Regex("\\B\\-(\\d+)$");
+            m = minus.Match(input);
+            if (m.Success)
+            {
+                return Tuple.Create(m.Value, -1 * int.Parse(m.Groups[1].Value));
+            }
+
+            return Tuple.Create(string.Empty, 0);
+        }
+
+        public static List<string> HashTags(string input)
+        {
+            Regex r = new Regex("#(.+?)\\b");
+            List<string> result = new List<string>();
+            Match m = r.Match(input);
+            while (m.Success)
+            {
+                result.Add(m.Groups[0].Value);
+                m = m.NextMatch();
+            }
+            return result;
+        }
+
+        public static Tuple<string, DateTime?, DateTime?> Date(string input)
+        {
+            DateTime? datetime = null;
+            Match m;
+
+            // PERIOD TASKS
+            Regex fromto = new Regex("\\bfrom\\s("+ DateTimeRE + ")\\sto\\s(" + DateTimeRE + ")\\b");
+
+            m = fromto.Match(input);
+            if (m.Success)
+            {
+                var matches = m.Groups;
+                DateTime? start = ParseDate(matches[1].Value);
+                DateTime? end = ParseDate(matches[2].Value);
+                return Tuple.Create(m.Value, start, end);
+            }
+
+            // DEADLINE TASKS
+            Regex deadline = new Regex("\\b(?:(?:by|due|on)\\s)?("+ DateTimeRE +")", RegexOptions.IgnoreCase);
+            m = deadline.Match(input);
+            if (m.Success)
+            {
+                var matches = m.Groups;
+
+                var x = new List<string>();
+                for (int i = 0; i < matches.Count; i++)
+                {
+                    x.Add(matches[i].Value);
+                }
+
+                DateTime? end = ParseDate(matches[1].Value);
+                return Tuple.Create(m.Value, datetime, end);
+            }
+
+            // if none of the regexp matches
+            // return the empty string.
+            return Tuple.Create(String.Empty, datetime, datetime);
+        }
+
+        private static string[] SanitizeToken(string value, string match, char split)
+        {
+            // check if keyword is matched
+            Regex keyword = new Regex(match, RegexOptions.IgnoreCase);
+            if (keyword.IsMatch(value))
+            {
+                // strip out keyword for date processing
+                var tmp = new List<string>(value.Split(' '));
+                tmp.RemoveAt(0);
+                value = String.Join(" ", tmp.ToArray());
+            }
+            return value.Split(split);
+        }
+
+        private static int MonthFromString(string monthToken)
+        {
+            Dictionary<string, int> monthDict = new Dictionary<string, int>();
+            monthDict.Add("january", 1);
+            monthDict.Add("febuary", 2);
+            monthDict.Add("march", 3);
+            monthDict.Add("april", 4);
+            monthDict.Add("may", 5);
+            monthDict.Add("june", 6);
+            monthDict.Add("july", 7);
+            monthDict.Add("august", 8);
+            monthDict.Add("september", 9);
+            monthDict.Add("october", 10);
+            monthDict.Add("november", 11);
+            monthDict.Add("december", 12);
+            monthDict.Add("jan", 1);
+            monthDict.Add("feb", 2);
+            monthDict.Add("mar", 3);
+            monthDict.Add("apr", 4);
+            monthDict.Add("jun", 6);
+            monthDict.Add("jul", 7);
+            monthDict.Add("aug", 8);
+            monthDict.Add("sep", 9);
+            monthDict.Add("oct", 10);
+            monthDict.Add("nov", 11);
+            monthDict.Add("dec", 12);
+
+            int month;
+            monthDict.TryGetValue(monthToken.ToLower(), out month);
+            return month;
+        }
+
+        private static int GetYearFromToken(string yearToken, int year)
+        {
+            if (yearToken.Length == 4)
+            {
+                year = int.Parse(yearToken);
+            }
+            else
+            {
+                int twoDigitYear = year % 100;
+                int yearTokenValue = int.Parse(yearToken);
+                if (yearTokenValue > twoDigitYear)
+                {
+                    year = 1900 + yearTokenValue;
+                }
+                else
+                {
+                    year = 2000 + yearTokenValue;
+                }
+            }
+            return year;
+        }
+
+        private static DateTime? ParseDate(string input)
+        {
+            Match m;
+            DateTime? datetime = null;
+            Regex ddmm = new Regex(DATE1, RegexOptions.IgnoreCase);
+            m = ddmm.Match(input);
+            if (m.Success)
+            {
+                string[] tokens = input.Split('/');
+                int date = int.Parse(tokens[0]);
+                int month = int.Parse(tokens[1]);
+                int year = DateTime.Today.Year;
+                if (tokens.Length == 3)
+                {
+                    year = GetYearFromToken(tokens[2], year);
+                }
+
+                try
+                {
+                    return new DateTime(year, month, date);
+                }
+                catch (Exception e)
+                {
+                    // invalid date. dont parse further.
+                    // do nothing.
+                }
+            }
+
+            Regex ddMonthyyyy = new Regex(DATE2, RegexOptions.IgnoreCase);
+            m = ddMonthyyyy.Match(input);
+            if (m.Success)
+            {
+                string[] tokens = input.Split(' ');
+                int date = int.Parse(tokens[0]);
+                int month = MonthFromString(tokens[1]);
+                int year = DateTime.Today.Year;
+                if (tokens.Length == 3)
+                {
+                    year = GetYearFromToken(tokens[2], year);
+                }
+
+                try
+                {
+                    return new DateTime(year, month, date);
+                }
+                catch (Exception e)
+                {
+                    // invalid date. dont parse further.
+                    // do nothing.
+                }
+            }
+
+            // if none of the regexp matches
+            // return the empty string.
+            return datetime;
+        }
+
+        public static DateTime? ParseDateTime(string input)
+        {
+            DateTime? result = null;
+
+
+
+            return result;
+        }
+
+    }
+}
