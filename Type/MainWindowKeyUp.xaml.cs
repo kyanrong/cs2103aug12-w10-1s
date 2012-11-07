f8a987148b8709c23ef820cf0d051e8c5280d9bd
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-11-07 20:12:07 +0800

    Changed escape character to the standard one.

---
+++ Type/MainWindowKeyUp.xaml.cs
@@ -0,0 +1,312 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Type
+{
+    public partial class MainWindow : Window
+    {
+        private void HandleSendCommand()
+        {
+            //Parse input.
+            //var parseResult = Command.Parse(inputBox.Text);
+
+            switch (parseResult.CommandText)
+            {
+                case Command.Invalid:
+                    invalidCmdPopup.IsOpen = true;
+                    break;
+
+                case Command.Search:
+                    DoSearch(parseResult);
+                    break;
+
+                case Command.Add:
+                    DoAdd(parseResult);
+                    break;
+
+                case Command.Help:
+                    DoHelp();
+                    break;
+
+                case Command.Archive:
+                case Command.Done:
+                case Command.Undo:
+                case Command.Edit:
+                case Command.Clear:
+                    DoGenericCommand(parseResult);
+                    break;
+                    
+                default:
+                    break;
+            }
+
+            //Retrieve a list of tasks, unless the list has already been retrieved by Search.
+            if (parseResult.CommandText != Command.Search)
+            {
+                isOriginalTasks = true;
+                renderedTasks = GetTasks(NUMBER_OF_TASKS_LOADED);
+            }
+
+            RenderTasks();
+        }
+
+        // @author A0092104
+        private void HandleAutoComplete()
+        {
+            //AutoComplete is only defined if there are rendered tasks on screen.
+            if (renderedTasks != null && renderedTasks.Count > 0)
+            {
+                //var parseResult = Command.Parse(inputBox.Text);
+
+                // If the command is Invalid, we try to autocomplete the command.
+                // Otherwise, we complete the task.
+                if (parseResult.CommandText != Command.Invalid && !parseResult.IsAlias)
+                {
+                    // If the input text is just the command, we append a space so that 
+                    // the user can continue typing.
+                    // Otherwise, we complete the partially written task.
+                    int completeBegin;
+                    if (inputBox.Text.EndsWith(parseResult.CommandText))
+                    {
+                        inputBox.Text += " ";
+                        MoveCursorToEndOfWord();
+                    }
+                    else if ((completeBegin =  LCPIndex(parseResult.Text, renderedTasks[0].RawText)) >= 0)
+                    {
+                        inputBox.Text += renderedTasks[0].RawText.Substring(completeBegin + 1);
+                        MoveCursorToEndOfWord();
+                    }
+                }
+                else
+                {
+                    inputBox.Text += Command.TryComplete(inputBox.Text.Substring(1));
+                    MoveCursorToEndOfWord();
+                }
+            }
+        }
+
+        // @author A0092104U
+        private void HandleEscapeKey()
+        {
+            // If we are highlighting something, we stop highlighting, but do not hide the window;
+            // We also refresh the view so that the highlight no longer shows.
+            // If the input box is not empty, we clear the input box, but do not hide the window.
+            // Otherwise, we hide the window.
+            // If the input box contains only whitespace (which will not be caught by the first condition),
+            // we clear it before hiding the window.
+            if (isHighlighting)
+            {
+                StopHighlighting();
+                RefreshViewList();
+            }
+            else if (inputBox.Text.Trim() != string.Empty)
+            {
+                inputBox.Clear();
+            }
+            else
+            {
+                if (inputBox.Text.Trim() == string.Empty)
+                {
+                    inputBox.Clear();
+                }
+                this.Hide();
+            }
+        }
+
+        // @author A0092104
+        private void StartHighlighting()
+        {
+            highlightIndex = 0;
+            isHighlighting = true;
+            ResetSelection();
+        }
+
+        // @author A0092104
+        private void StopHighlighting()
+        {
+            isHighlighting = false;
+            ResetSelection();
+        }
+
+        // @author A0092104
+        private void ResetSelection()
+        {
+            // We have a non-ambiguous match iff there is exactly one task rendered.
+            // Otherwise, set the selectedTask to null to represent no task selected.
+            selectedTask = renderedTasks.Count == 1 ? renderedTasks[0] : null;
+        }
+
+        //@author A0088574M
+        //modify the highlight index and may go to the previous page.
+        private void HandleUpArrow()
+        {
+            if (!isHighlighting && listStartIndex != 0)
+            {
+                StartHighlighting();
+            }
+            
+            highlightIndex--;
+
+            if (highlightIndex < 0 && listStartIndex == 0)
+            {
+                StopHighlighting();
+            }
+
+            //when highlighIndex out of bound and current page is not the first page
+            if (highlightIndex < 0 && listStartIndex > 0)
+            {
+                MoveToPreviousPage();
+                highlightIndex = (listEndIndex - 1) % NUMBER_OF_TASKS_DISPLAYED;
+            }
+
+            CheckHighlightIndexBound();
+
+            RefreshViewList();
+            //inputBox.Text = selectedTaskText;
+        }
+
+        //modify the highlightIndex and may go to next page
+        private void HandleDownArrow()
+        {
+            if (!isHighlighting)
+            {
+                StartHighlighting();
+            }
+            else
+            {
+                highlightIndex++;
+            }
+
+            if ((highlightIndex > NUMBER_OF_TASKS_DISPLAYED-1) && (listEndIndex != renderedTasks.Count))
+            {
+                MoveToNextPage();
+                highlightIndex = 0;
+            }
+
+            CheckHighlightIndexBound();
+
+            RefreshViewList();
+            //inputBox.Text = selectedTaskText;
+        }
+        
+        private void HandleLeftArrow()
+        {
+            //in case user only want to move the cursor in the text box, not the page
+            //but user can still use the left key when searching for task in filter list
+            if (parseResult.CommandText == Command.Search || inputBox.Text == string.Empty)
+            {
+                MoveToPreviousPage();
+            }
+        }
+        
+        private void HandleRightArrow()
+        {
+            //in case user only want to move the cursor in the text box, not the page
+            //but user can still use the right key when searching for task in filter list
+            if (parseResult.CommandText == Command.Search || inputBox.Text == string.Empty)
+            {
+                MoveToNextPage();
+            }
+        }
+
+        //go to next page, will modify listStartIndex and listEndIndex
+        //may modify highlightIndex
+        private void MoveToNextPage()
+        {
+            //already at the last page
+            if (listEndIndex == renderedTasks.Count)
+            {
+                return;
+            }
+
+            listEndIndex += NUMBER_OF_TASKS_DISPLAYED;
+            listStartIndex = listEndIndex - NUMBER_OF_TASKS_DISPLAYED;
+
+            CheckListIndexBound();
+            CheckHighlightIndexBound();
+
+            ResetSelection();
+
+            RefreshViewList();
+        }
+
+        //go to previous page, will modify listStartIndex and listEndIndex
+        //may modify highlightIndex
+        private void MoveToPreviousPage()
+        {
+            //already at the first page
+            if (listStartIndex == 0)
+            {
+                return;
+            }
+
+            listStartIndex -= NUMBER_OF_TASKS_DISPLAYED;
+            listEndIndex = listStartIndex + NUMBER_OF_TASKS_DISPLAYED;
+
+            CheckListIndexBound();
+            CheckHighlightIndexBound();
+
+            ResetSelection();
+
+            RefreshViewList();
+        }
+
+        //if the list index is out of bound then set it back to the correct bound
+        private void CheckListIndexBound()
+        {
+            if (listStartIndex < 0)
+            {
+                listStartIndex = 0;
+            }
+
+            if (listEndIndex > renderedTasks.Count)
+            {
+                listEndIndex = renderedTasks.Count;
+            }
+        }
+
+        //if the highLightIndex out of bound the set it back to the correct bound
+        private void CheckHighlightIndexBound()
+        {
+            if (highlightIndex < 0)
+            {
+                highlightIndex = 0;
+            }
+            if (highlightIndex > (listEndIndex-1) % NUMBER_OF_TASKS_DISPLAYED)
+            {
+                highlightIndex = (listEndIndex-1) % NUMBER_OF_TASKS_DISPLAYED;
+            }
+        }
+        
+        // Used for auto complete.
+        private void MoveCursorToEndOfWord()
+        {
+            inputBox.Select(inputBox.Text.Length, 0);
+        }
+
+        // @author A0092104
+        // Finds the longest common prefix of a and b.
+        private int LCPIndex(string a, string b)
+        {
+            var found = -1;
+            var commonLength = Math.Min(a.Length, b.Length);
+            for (int i = 0; i < commonLength; i++)
+            {
+                if (a[i] == b[i])
+                {
+                    found = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return found;
+        }
+    }
+}
---
+++ Type/RegExp.cs
@@ -0,0 +1,394 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Type
+{
+    public class RegExp
+    {
+        // date re
+        // 1. DDMM[YY[YY]]
+        public static string DATE1 = "\\b\\d{1,2}\\/\\d{1,2}(?:\\/\\d{2,4})?\\b";
+        // 2 DD string_rep_of_month [YY[YY]]
+        public static string DATE2 = "\\b\\d{1,2}\\s(?:january|febuary|march|april|may|june|july|august|september|october|november|december|jan|feb|mar|apr|may|jun|jul|aug|sep|oct|nov|dec)(?:\\s\\d{2,4})?\\b";
+        
+        // 3 Nice Dates
+        public static string DATE3 = "\\btoday|tdy\\b";
+        public static string DATE4 = "\\btomorrow|tmr\\b";
+        
+        // time re
+        // 1. NN(am|pm)
+        public static string TIME1 = "\\b\\d{1,2}(?:am|pm)\\b";
+        // 2. NN:NN[am|pm]
+        public static string TIME2 = "\\b\\d{1,2}:\\d{2}(?:am|pm)?\\b";
+
+        // Combine cases.
+        public static string DateRE = "(?:" + DATE1 + "|" + DATE2 + "|" + DATE3 + "|" + DATE4 + ")";
+        public static string TimeRE = "(?:" + TIME1 + "|" + TIME2 + ")";
+
+        // datetime re
+        // 1. date [time]
+        public static string DateTime1 = DateRE + "(?:\\s" + TimeRE + ")?";
+        // 2. time [date]
+        public static string DateTime2 = TimeRE + "(?:\\s" + DateRE + ")?";
+
+        // Combine cases
+        public static string DateTimeRE = DateTime1 + "|" + DateTime2;
+
+
+        public static Tuple<string, int> Priority(string input)
+        {
+            Match m;
+            Regex plus = new Regex("\\B\\+(\\d+)$");
+            m = plus.Match(input);
+            if (m.Success)
+            {
+                return Tuple.Create(m.Value, int.Parse(m.Groups[1].Value));
+            }
+            
+            Regex minus = new Regex("\\B\\-(\\d+)$");
+            m = minus.Match(input);
+            if (m.Success)
+            {
+                return Tuple.Create(m.Value, -1 * int.Parse(m.Groups[1].Value));
+            }
+
+            return Tuple.Create(string.Empty, 0);
+        }
+
+        public static List<string> HashTags(string input)
+        {
+            Regex r = new Regex("#(.+?)\\b");
+            List<string> result = new List<string>();
+            Match m = r.Match(input);
+            while (m.Success)
+            {
+                result.Add(m.Groups[0].Value);
+                m = m.NextMatch();
+            }
+            return result;
+        }
+
+
+        public static Tuple<string, DateTime?, DateTime?> DateTimeT(string input, DateTime today)
+        {
+            DateTime? datetime = null;
+            Match m;
+
+            // PERIOD TASKS
+            Regex fromto = new Regex("\\bfrom\\s("+ DateTimeRE + ")\\sto\\s(" + DateTimeRE + ")\\b");
+
+            m = fromto.Match(input);
+            if (m.Success)
+            {
+                var matches = m.Groups;
+                DateTime? start = ParseDateTime(matches[1].Value, today);
+                DateTime? end = ParseDateTime(matches[2].Value, today);
+                return Tuple.Create(m.Value, start, end);
+            }
+
+            // DEADLINE TASKS
+            Regex deadline = new Regex("\\b(?:(?:by|due|on)\\s)?("+ DateTimeRE +")", RegexOptions.IgnoreCase);
+            m = deadline.Match(input);
+            if (m.Success)
+            {
+                var matches = m.Groups;
+                DateTime? end = ParseDateTime(matches[1].Value, today);
+                return Tuple.Create(m.Value, datetime, end);
+            }
+
+            // if none of the regexp matches
+            // return the empty string.
+            return Tuple.Create(String.Empty, datetime, datetime);
+        }
+
+        public static Tuple<string, DateTime?, DateTime?> DateTimeT(string input)
+        {
+            return DateTimeT(input, DateTime.Today);
+        }
+
+        private static string[] SanitizeToken(string value, string match, char split)
+        {
+            // check if keyword is matched
+            Regex keyword = new Regex(match, RegexOptions.IgnoreCase);
+            if (keyword.IsMatch(value))
+            {
+                // strip out keyword for date processing
+                var tmp = new List<string>(value.Split(' '));
+                tmp.RemoveAt(0);
+                value = String.Join(" ", tmp.ToArray());
+            }
+            return value.Split(split);
+        }
+
+        private static int MonthFromString(string monthToken)
+        {
+            Dictionary<string, int> monthDict = new Dictionary<string, int>();
+            monthDict.Add("january", 1);
+            monthDict.Add("febuary", 2);
+            monthDict.Add("march", 3);
+            monthDict.Add("april", 4);
+            monthDict.Add("may", 5);
+            monthDict.Add("june", 6);
+            monthDict.Add("july", 7);
+            monthDict.Add("august", 8);
+            monthDict.Add("september", 9);
+            monthDict.Add("october", 10);
+            monthDict.Add("november", 11);
+            monthDict.Add("december", 12);
+            monthDict.Add("jan", 1);
+            monthDict.Add("feb", 2);
+            monthDict.Add("mar", 3);
+            monthDict.Add("apr", 4);
+            monthDict.Add("jun", 6);
+            monthDict.Add("jul", 7);
+            monthDict.Add("aug", 8);
+            monthDict.Add("sep", 9);
+            monthDict.Add("oct", 10);
+            monthDict.Add("nov", 11);
+            monthDict.Add("dec", 12);
+
+            int month;
+            monthDict.TryGetValue(monthToken.ToLower(), out month);
+            return month;
+        }
+
+        private static int GetYearFromToken(string yearToken)
+        {
+            int year = DateTime.Today.Year;
+            if (yearToken.Length == 4)
+            {
+                year = int.Parse(yearToken);
+            }
+            else
+            {
+                int twoDigitYear = year % 100;
+                int yearTokenValue = int.Parse(yearToken);
+                if (yearTokenValue > twoDigitYear)
+                {
+                    year = 1900 + yearTokenValue;
+                }
+                else
+                {
+                    year = 2000 + yearTokenValue;
+                }
+            }
+            return year;
+        }
+
+        // extracts information from a date string and returns it in a tuple
+        public static Tuple<int, int, int> DateFromDateString(string input, DateTime today)
+        {
+            // case 1.
+            // DD/MM[/YY[YY]]
+            Match m;
+            Regex re1 = new Regex(DATE1, RegexOptions.IgnoreCase);
+            m = re1.Match(input);
+            if (m.Success)
+            {
+                string[] tokens = input.Split('/');
+                int date = int.Parse(tokens[0]);
+                int month = int.Parse(tokens[1]);
+
+                int year = DateTime.Today.Year;
+                if (tokens.Length == 3)
+                {
+                    year = GetYearFromToken(tokens[2]);
+                }
+                else
+                {
+                    // check if year is this year/next
+                    DateTime res = new DateTime(year, month, date);
+                    if (res < DateTime.Today)
+                    {
+                        year = year + 1;
+                    }
+                }
+                return Tuple.Create(year, month, date);
+            }
+
+            // case 2.
+            // DD string_rep_of_month [YY[YY]]
+            Regex re2 = new Regex(DATE2, RegexOptions.IgnoreCase);
+            m = re2.Match(input);
+            if (m.Success)
+            {
+                string[] tokens = input.Split(' ');
+                int date = int.Parse(tokens[0]);
+                int month = MonthFromString(tokens[1]);
+                int year = DateTime.Today.Year;
+                if (tokens.Length == 3)
+                {
+                    year = GetYearFromToken(tokens[2]);
+                }
+                else
+                {
+                    // check if year is this year/next
+                    DateTime res = new DateTime(year, month, date);
+                    if (res < DateTime.Today)
+                    {
+                        year = year + 1;
+                    }
+                }
+                return Tuple.Create(year, month, date);
+            }
+
+            // case 3.
+            // nice, today
+            Regex re3 = new Regex(DATE3, RegexOptions.IgnoreCase);
+            m = re3.Match(input);
+            if (m.Success)
+            {
+                int date = today.Day;
+                int month = today.Month;
+                int year = today.Year;
+                return Tuple.Create(year, month, date);
+            }
+
+            // case 4.
+            // nice, today
+            Regex re4 = new Regex(DATE4, RegexOptions.IgnoreCase);
+            m = re4.Match(input);
+            if (m.Success)
+            {
+                DateTime tmr = today.AddDays(1);
+                int date = tmr.Day;
+                int month = tmr.Month;
+                int year = tmr.Year;
+                return Tuple.Create(year, month, date);
+            }
+
+            // invalid match.
+            // return invalid date tuple
+            return Tuple.Create(-1, -1, -1);
+        }
+
+        public static Tuple<int, int, int> DateFromDateString(string input)
+        {
+            return DateFromDateString(input, DateTime.Today);
+        }
+        // extracts information from time string and returns it in a tuple
+        public static Tuple<int, int> TimeFromTimeString(string input)
+        {
+            // case 1.
+            // HH[:MM][(am|pm)]
+            Match m;
+            Regex re1 = new Regex("([0-9:]+)(am|pm)?", RegexOptions.IgnoreCase);
+            m = re1.Match(input);
+            if (m.Success)
+            {
+                var matches = m.Groups;
+                
+                // hours and minutes
+                string[] tokens = matches[1].Value.Split(':');
+                int hour = int.Parse(tokens[0]);
+                int minutes = tokens.Length == 1 ? 0 : int.Parse(tokens[1]); 
+
+                // AM or PM
+                if (matches[2].Value == "am")
+                {
+                    if (hour == 12)
+                    {
+                        hour = 0;
+                    }
+                }
+                else if (matches[2].Value == "pm")
+                {
+                    if (hour < 12)
+                    {
+                        hour = hour + 12;
+                    }
+                }
+
+                return Tuple.Create(hour, minutes);
+            }
+
+
+            
+            // invalid match.
+            // return invalid time tuple
+            return Tuple.Create(-1, -1);
+        }
+
+        // returns datetime object if input string matches one of the acceptable formats
+        private static DateTime? ParseDateTime(string input, DateTime today)
+        {
+            // booleans to keep track if either date/time is matched.
+            // either one is necessary for input to qualify as a date/time
+            bool date = false;
+            bool time = false;
+            DateTime result = today;
+
+            Match m;
+            // get date information
+            Regex getDate = new Regex(DateRE, RegexOptions.IgnoreCase);
+            m = getDate.Match(input);
+            if (m.Success)
+            {
+                date = true;
+                string dateString = m.Groups[0].Value;
+                var dateTuple = DateFromDateString(dateString, today);
+
+                int year = dateTuple.Item1;
+                int month = dateTuple.Item2;
+                int day = dateTuple.Item3;
+
+                try
+                {
+                    result = new DateTime(year, month, day);
+                }
+                catch (Exception e)
+                {
+                    // invalid date.
+                    // set date match to false.
+                    date = false;
+                }
+            }
+
+            // get time information
+            Regex getTime = new Regex(TimeRE, RegexOptions.IgnoreCase);
+            m = getTime.Match(input);
+            if (m.Success)
+            {
+                time = true;
+                string timeString = m.Groups[0].Value;
+                var timeTuple = TimeFromTimeString(timeString);
+
+                int hour = timeTuple.Item1;
+                int minutes = timeTuple.Item2;
+
+                try
+                {
+                    int day = result.Day;
+                    int month = result.Month;
+                    int year = result.Year;
+                    result = new DateTime(year, month, day, hour, minutes, 0);
+                }
+                catch (Exception e)
+                {
+                    // invalid time
+                    // set time match to false
+                    time = false;
+                }
+            }
+
+            // none of the regexp matches
+            if (!date && !time)
+            {
+                return null;
+            }
+            else
+            {
+                return result;
+            }
+        }
+
+        private static DateTime? ParseDateTime(string input)
+        {
+            return ParseDateTime(input, DateTime.Today);
+        }
+    }
+}
