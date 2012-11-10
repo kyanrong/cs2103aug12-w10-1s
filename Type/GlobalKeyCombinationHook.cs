6add9707a151843f64ad6565f5f1bf2ad09fdcee
Camillus Gerard Cai <cgcai@comp.nus.edu.sg>
2012-11-11 00:11:41 +0800

    More tagging.

---
+++ Type/GlobalKeyCombinationHook.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Interop;
+
+namespace Type
+{
+    #region Delegates
+    public delegate void ShortcutPressedEventHandler(object sender, EventArgs e);
+    #endregion
+
+    //@author A0092104U
+    class GlobalKeyCombinationHook
+    {
+        #region Constants
+        private const string ATOM_NAME = "TypeShortcut";
+        #endregion
+
+        #region Fields
+        //Handles and references to a System.Windows.Window for listening to system messages.
+        private Window targetWindow;
+        private HwndSource targetWindowHandle;
+        private short atom;    
+
+        //Key combination to catch.
+        private uint modifier;
+        private uint vkey;
+
+        #endregion
+
+        #region Events
+        public event ShortcutPressedEventHandler ShortcutPressed;
+        #endregion
+
+        #region Event Methods
+        protected virtual void OnShortcutPressed(EventArgs e)
+        {
+            if (ShortcutPressed != null) ShortcutPressed(this, e);
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Creates a GlobalKeyCombination Hook object.
+        /// </summary>
+        /// <param name="targetWindow">Reference to a System.Windows.Window.</param>
+        /// <param name="combinationHandler">Callback that receives key combination messages.</param>
+        /// <param name="modifier">Represents the combination of modifier keys to catch.</param>
+        /// <param name="vkey">Represents the VIRTUAL_KEY value fo the key to catch.</param>
+        public GlobalKeyCombinationHook(Window targetWindow, uint modifier, uint vkey)
+        {
+            this.targetWindow = targetWindow;
+
+            this.modifier = modifier;
+            this.vkey = vkey;
+        }
+
+        /// <summary>
+        /// Creates a keyboard hook and starts listening.
+        /// </summary>
+        /// <returns>Self-reference.</returns>
+        public GlobalKeyCombinationHook StartListening()
+        {
+            //The window only gets a hwnd after it is shown the first time.
+            //If the window has been shown before, the following has no side-effects;
+            //Otherwise, a hwnd is created by showing an invisible window.
+            var targetProperties = StoreProperties(targetWindow);
+            MakeInvisible(targetWindow);
+            targetWindow.Show();
+
+            WindowInteropHelper wih = AddHook();
+            atom = CreateAtom();
+            RegisterCombination(wih);
+
+            //We no longer need the window after the hook and callback have been registered.
+            //Hide it and restore its original properties.
+            targetWindow.Hide();
+            RestoreProperties(targetWindow, targetProperties);
+
+            return this;
+        }
+
+        //Restores the visual state of a System.Windows.Window.
+        private void RestoreProperties(Window win, Tuple<double, double, WindowStyle, bool, bool> prop)
+        {
+            win.Height = prop.Item1;
+            win.Width = prop.Item2;
+            win.WindowStyle = prop.Item3;
+            win.ShowInTaskbar = prop.Item4;
+            win.ShowActivated = prop.Item5;
+        }
+
+        //Makes a System.Windows.Window visually invisible.
+        private void MakeInvisible(Window win)
+        {
+            win.Height = 0;
+            win.Width = 0;
+            win.WindowStyle = WindowStyle.None;
+            win.ShowInTaskbar = false;
+            win.ShowActivated = false;
+        }
+
+        //Temporarily stores the visual state of a System.Windows.Window.
+        private Tuple<double, double, WindowStyle, bool, bool> StoreProperties(Window win)
+        {
+            double height = targetWindow.Height;
+            double width = targetWindow.Width;
+            WindowStyle style = targetWindow.WindowStyle;
+            bool taskbar = targetWindow.ShowInTaskbar;
+            bool activation = targetWindow.ShowActivated;
+
+            return new Tuple<double, double, WindowStyle, bool, bool>(height, width, style, taskbar, activation);
+        }
+
+        /// <summary>
+        /// Removes the keyboard hook.
+        /// </summary>
+        /// <returns>Self reference.</returns>
+        public GlobalKeyCombinationHook StopListening()
+        {
+            UnregisterCombination();
+
+            return this;
+        }
+
+        //Unregister a previously registered atom with the host OS.
+        private void UnregisterCombination()
+        {
+            if (atom != 0)
+            {
+                UnregisterHotKey(targetWindowHandle.Handle, atom);
+            }
+        }
+
+        //Add a hook to listen to messages from the host OS.
+        private WindowInteropHelper AddHook()
+        {
+            var wih = new WindowInteropHelper(targetWindow);
+
+            targetWindowHandle = HwndSource.FromHwnd(wih.Handle);
+            targetWindowHandle.AddHook(HotKeyMsgHandler);
+
+            return wih;
+        }
+
+        //Registers the combination that was initialized with this object with the host OS.
+        private void RegisterCombination(WindowInteropHelper wih)
+        {
+            bool result = RegisterHotKey(wih.Handle, atom, modifier, vkey);
+
+            if (!result)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+        }
+
+        //Create an atom in win32 to receive system messages.
+        private short CreateAtom()
+        {
+            short atom = GlobalAddAtom(ATOM_NAME);
+
+            if (atom == 0)
+            {
+                throw new Win32Exception(Marshal.GetLastWin32Error());
+            }
+
+            return atom;
+        }
+
+        //Handles system messages.
+        //Calls the callback that was initialized with this object if the message is WM_HOTKEY.
+        private IntPtr HotKeyMsgHandler(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            if (msg == GlobalKeyCombinationHook.WM_HOTKEY)
+            {
+                OnShortcutPressed(EventArgs.Empty);
+                handled = true;
+            }
+
+            return IntPtr.Zero;
+        }
+        #endregion
+
+        #region Win32 Functions
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern short GlobalAddAtom(string lpString);
+
+        [DllImport("kernel32", SetLastError = true)]
+        public static extern short GlobalDeleteAtom(short nAtom);
+        #endregion
+
+        #region Win32 Constants
+        //fsModifiers defined by Win32 RegisterHotKey
+        public const uint MOD_ALT = 0x0001;
+        public const uint MOD_CONTROL = 0x0002;
+        public const uint MOD_SHIFT = 0x0004;
+        public const uint MOD_WIN = 0x0008;
+        public const uint MOD_NOREPEAT = 0x4000;
+
+        //WM_HOTKEY value
+        private const int WM_HOTKEY = 0x0312;
+        #endregion
+    }
+}
---
+++ Type/MainWindow_UILogic.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Type
+{
+    public partial class MainWindow : Window
+    {
+        #region Command Handlers
+        private void HandleSendCommand()
+        {
+            switch (parseResult.CommandText)
+            {
+                case Command.Invalid:
+                    invalidCmdPopup.IsOpen = true;
+                    break;
+
+                case Command.Search:
+                    DoSearch(parseResult);
+                    break;
+
+                case Command.Add:
+                    DoAdd(parseResult);
+                    break;
+
+                case Command.Help:
+                    DoHelp();
+                    break;
+
+                case Command.Archive:
+                case Command.Done:
+                case Command.Undo:
+                case Command.Edit:
+                case Command.Clear:
+                    DoGenericCommand(parseResult);
+                    break;
+
+                default:
+                    break;
+            }
+
+            // Retrieve a list of tasks, unless the list has already been retrieved by Search.
+            if (parseResult.CommandText != Command.Search)
+            {
+                isOriginalTasks = true;
+                renderedTasks = GetTasks(NUMBER_OF_TASKS_LOADED);
+            }
+
+            RenderTasks();
+        }
+
+        //@author A0092104U
+        private void DoEdit(Task selectedTask)
+        {
+            //Populate inputBox with edit text.
+            if (selectedTask != null)
+            {
+                inputBox.Text = selectedTask.RawText;
+            }
+            MoveCursorToEndOfWord();
+        }
+
+        //@author A0092104U
+        private void DoSearch(Command result)
+        {
+            if (result.Text.Trim() != string.Empty)
+            {
+                isOriginalTasks = false;
+                renderedTasks = GetTasksByHashTag(result.Text.Trim());
+            }
+        }
+
+        //@author A0083834Y
+        private void DoHelp()
+        {
+            helpDescriptionListBox.DataContext = helpDescription;
+
+            helpDescriptionPopup.IsOpen = true;
+        }
+
+        //@author A0092104U
+        private void DoAdd(Command result)
+        {
+            //The default command is "add".
+            if (result.Text.Trim() != string.Empty)
+            {
+                OnRequestExecute(new CommandEventArgs(result.CommandText, result.Text));
+                inputBox.Clear();
+            }
+        }
+
+        //@author A0092104U
+        private void DoGenericCommand(Command result)
+        {
+            Task target = null;
+
+            if (renderedTasks.Count > 0)
+            {
+                target = selectedTask;
+
+                if (result.CommandText == Command.Edit)
+                {
+                    DoEdit(target);
+                }
+            }
+
+            OnRequestExecute(new CommandEventArgs(result.CommandText, result.Text, target));
+
+            // Clear the input box if the command was not edit.
+            // If the command was edit, and a null task was sent to the Presenter,
+            // then it was an invalid edit command. Clear the input box.
+            if (result.CommandText != Command.Edit)
+            {
+                inputBox.Clear();
+            }
+            else if (selectedTask == null)
+            {
+                inputBox.Clear();
+            }
+        }
+
+        //@author A0083834Y
+        private void PopulateHelpList()
+        {
+            // Populate helpDescription List
+            helpDescription.Add("Create new task");
+            helpDescription.Add("<task>");
+
+            helpDescription.Add("Complete a task");
+            helpDescription.Add(":done <task>");
+
+            helpDescription.Add("Complete all tasks with a tag");
+            helpDescription.Add(":done #<tag name>");
+
+            helpDescription.Add("Archive all completed tasks");
+            helpDescription.Add(":archive");
+
+            helpDescription.Add("Archive a single task");
+            helpDescription.Add(":archive <task>");
+
+            helpDescription.Add("Archive all tasks with a tag");
+            helpDescription.Add(":archive #<tag name>");
+
+            helpDescription.Add("Edit a task");
+            helpDescription.Add(":edit <task>");
+
+            helpDescription.Add("Undo last action");
+            helpDescription.Add(":undo");
+
+            helpDescription.Add("Filter by hash tags");
+            helpDescription.Add("/<tag name> [<tag name>] ...");
+
+            helpDescription.Add("Show archived tasks");
+            helpDescription.Add("/archive <tag name> [<tag name>] ...");
+
+            helpDescription.Add("Sort the display");
+            helpDescription.Add(":sort <field>");
+        }
+        #endregion
+
+        #region AutoComplete
+        //@author A0092104U
+        private void HandleAutoComplete()
+        {
+            const string SPACE = " ";
+
+            // AutoComplete is only defined if there are rendered tasks on screen.
+            if (renderedTasks != null && renderedTasks.Count > 0)
+            {
+                // If the command is Invalid, we try to autocomplete the command.
+                // Otherwise, we complete the task.
+                if ((parseResult.CommandText == Command.Invalid || parseResult.IsAlias) && parseResult.Text == string.Empty)
+                {
+                    inputBox.Text += (Command.TryComplete(inputBox.Text.Substring(1)) + SPACE);
+                    MoveCursorToEndOfWord();
+                }
+                else
+                {
+                    // If the input text is just the command, we append a space so that 
+                    // the user can continue typing.
+                    // Otherwise, we complete the partially written task.
+                    int completeBegin;
+                    if (inputBox.Text.EndsWith(parseResult.CommandText))
+                    {
+                        inputBox.Text += SPACE;
+                        MoveCursorToEndOfWord();
+                    }
+                    else if ((completeBegin = LCPIndex(parseResult.Text, renderedTasks[0].RawText)) >= 0)
+                    {
+                        inputBox.Text = inputBox.Text.Trim() + renderedTasks[0].RawText.Substring(completeBegin + 1);
+                        MoveCursorToEndOfWord();
+                    }
+                }
+            }
+        }
+
+        //@author A0092104U
+        // Finds the longest common prefix of a and b.
+        private int LCPIndex(string a, string b)
+        {
+            var found = -1;
+            var commonLength = Math.Min(a.Length, b.Length);
+            for (int i = 0; i < commonLength; i++)
+            {
+                if (a[i] == b[i])
+                {
+                    found = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return found;
+        }
+        #endregion
+
+        #region Context Escape
+        //@author A0092104U
+        private void HandleEscapeKey()
+        {
+            // If we are highlighting something, we stop highlighting, but do not hide the window;
+            // We also refresh the view so that the highlight no longer shows.
+            // If the input box is not empty, we clear the input box, but do not hide the window.
+            // Otherwise, we hide the window.
+            // If the input box contains only whitespace (which will not be caught by the first condition),
+            // we clear it before hiding the window.
+            if (isHighlighting)
+            {
+                StopHighlighting();
+                RefreshViewList();
+            }
+            else if (inputBox.Text.Trim() != string.Empty)
+            {
+                inputBox.Clear();
+            }
+            else
+            {
+                if (inputBox.Text.Trim() == string.Empty)
+                {
+                    inputBox.Clear();
+                }
+                this.Hide();
+            }
+        }
+        #endregion
+
+        #region Selection Methods
+        //@author A0088574M
+        private void InitializeListBounderIndex()
+        {
+            highlightListIndex = 0;
+            listStartIndex = 0;
+
+            if (renderedTasks.Count > NUMBER_OF_TASKS_DISPLAYED)
+            {
+                listEndIndex = NUMBER_OF_TASKS_DISPLAYED;
+            }
+
+            else
+            {
+                listEndIndex = renderedTasks.Count;
+            }
+        }
+
+        //@author A0092104U
+        private void StartHighlighting()
+        {
+            highlightListIndex = 0;
+            isHighlighting = true;
+            ResetSelection();
+        }
+
+        //@author A0092104U
+        private void StopHighlighting()
+        {
+            isHighlighting = false;
+            ResetSelection();
+        }
+
+        //@author A0092104U
+        private void ResetSelection()
+        {
+            // We have a non-ambiguous match iff there is exactly one task rendered.
+            // Otherwise, set the selectedTask to null to represent no task selected.
+            selectedTask = renderedTasks.Count == 1 ? renderedTasks[0] : null;
+        }
+
+        //@author A0088574M
+        //if the list index is out of bound then set it back to the correct bound
+        private void CheckListIndexBound()
+        {
+            if (listStartIndex < 0)
+            {
+                listStartIndex = 0;
+            }
+
+            if (listEndIndex > renderedTasks.Count)
+            {
+                listEndIndex = renderedTasks.Count;
+            }
+        }
+
+        //if the highLightIndex out of bound the set it back to the correct bound
+        private void CheckHighlightIndexBound()
+        {
+            if (highlightListIndex < 0)
+            {
+                highlightListIndex = 0;
+            }
+            if (highlightListIndex > (listEndIndex - 1) % NUMBER_OF_TASKS_DISPLAYED)
+            {
+                highlightListIndex = (listEndIndex - 1) % NUMBER_OF_TASKS_DISPLAYED;
+            }
+        }
+        #endregion
+    }
+}
