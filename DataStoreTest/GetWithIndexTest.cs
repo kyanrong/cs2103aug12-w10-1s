7dafdd4650bace4f804c082d42bea7b45f5ce89b
Ang Civics <angcivics@gmail.com>
2012-11-11 22:47:04 +0800

    change done log

---
+++ DataStoreTest/GetWithIndexTest.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using Type;
+
+namespace DataStoreTest
+{
+    //@author A0088574M
+    [TestClass]
+    public class GetWithIndexTest
+    {     
+        [TestMethod]
+        [ExpectedException(typeof(MissingFieldException))]
+        //will throw an exception if the file is changed until the index is missing
+        //the missing index is checked before the target is found
+        public void GetWithIndexExceptionTest()
+        {
+            //create the file with data
+            DataStore myData = new DataStore("Exception_GetWithIndex");
+            List<string> myList = new List<string>();
+
+            myList.Add("meeting on 9/11");
+            myList.Add(true.ToString());
+            myList.Add(false.ToString());
+            myData.InsertRow(myList);
+
+            myList.Clear();
+            myList.Add("project deadline 12/nov #cs +100");
+            myList.Add("False");
+            myList.Add("True");
+            myData.InsertRow(myList);
+
+            myList.Clear();
+            myList.Add("007 skyfall");
+            myList.Add("False");
+            myList.Add(false.ToString());
+            myData.InsertRow(myList);
+
+            //change the file data illegally
+            FileStream fs = new FileStream("Exception_GetWithIndex", FileMode.Truncate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            sw.WriteLine(",meeting on 9/11,True,True");
+            sw.WriteLine("2,project deadline 12/nov #cs +100,False,True");
+            sw.WriteLine("3,007 skyfall,False,False");
+
+            sw.Close();
+            fs.Close();
+
+            //testing, should throw exception here
+            myData.Get(2);
+        }
+
+        [TestMethod]
+        //if the target index is out of bound, will return null
+        public void OutOfBoundTest()
+        {
+            //create file with data
+            DataStore myData = new DataStore("OutOfBound_Get");
+            List<string> myList = new List<string>();
+
+            //normal list
+            myList.Add("meeting on 9/11");
+            myList.Add(true.ToString());
+            myList.Add(false.ToString());
+            myData.InsertRow(myList);
+
+            //list with empty string
+            myList.Clear();
+            myList.Add("");
+            myData.InsertRow(myList);
+
+            //empty list
+            myList.Clear();
+            myData.InsertRow(myList);
+
+            List<string> actual;
+            actual = myData.Get(4);//out of bound
+            Assert.AreEqual(null, actual);
+
+            actual = myData.Get(0);//out of bound
+            Assert.AreEqual(null, actual);
+
+            actual = myData.Get(1);//normal case
+            Assert.AreNotEqual(null, actual);
+
+            actual = myData.Get(2);//list with empty string
+            Assert.AreNotEqual(null, actual);
+
+            actual = myData.Get(3);//empty list
+            Assert.AreNotEqual(null, actual);
+        }
+
+        [TestMethod]
+        public void Normal_GetWithIndex()
+        {
+            //create file with data
+            DataStore myData = new DataStore("Normal_GetWithIndex");
+            List<string> myList1 = new List<string>(), myList2 = new List<string>(), myList3 = new List<string>();
+
+            myList1.Add("first");
+            myList1.Add("try");
+            myData.InsertRow(myList1);
+
+            myList2.Add("second");
+            myList2.Add("try");
+            myData.InsertRow(myList2);
+
+            myList3.Add("third");
+            myList3.Add("try");
+            myData.InsertRow(myList3);
+
+            //testing
+            List<string> actualList;
+            actualList = myData.Get(1);
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(myList1.ElementAt(i), actualList.ElementAt(i));
+            }
+
+            actualList = myData.Get(2);
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(myList2.ElementAt(i), actualList.ElementAt(i));
+            }
+            
+            actualList = myData.Get(3);
+            for (int i = 0; i < 2; i++)
+            {
+                Assert.AreEqual(myList3.ElementAt(i), actualList.ElementAt(i));
+            }
+        }
+    }
+}
---
+++ Type/MainWindow_Navigation.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace Type
+{
+    public partial class MainWindow : Window
+    {
+        #region Page Navigation Handlers
+        //@author A0088574M
+        //modify the highlight index and may go to the previous page.
+        private void HandleUpArrow()
+        {
+            //when not at the first page and not yet start highlighting
+            if (!isHighlighting && listStartIndex != 0)
+            {
+                StartHighlighting();
+            }
+
+            highlightListIndex--;
+
+            //when current highlight is at the first task
+            if (highlightListIndex < 0 && listStartIndex == 0)
+            {
+                StopHighlighting();
+            }
+
+            //when highlighIndex out of bound and current page is not the first page
+            if (highlightListIndex < 0 && listStartIndex > 0)
+            {
+                MoveToPreviousPage();
+                highlightListIndex = (listEndIndex - 1) % NUMBER_OF_TASKS_DISPLAYED;
+            }
+
+            CheckHighlightIndexBound();
+
+            RefreshViewList();
+        }
+
+        //modify the highlightIndex and may go to next page
+        private void HandleDownArrow()
+        {
+            if (!isHighlighting)
+            {
+                StartHighlighting();
+            }
+            else
+            {
+                highlightListIndex++;
+            }
+
+            //when hightlight index out of bound, and current page is not the last page
+            if ((highlightListIndex > NUMBER_OF_TASKS_DISPLAYED - 1) && (listEndIndex != renderedTasks.Count))
+            {
+                MoveToNextPage();
+                highlightListIndex = 0;
+            }
+
+            CheckHighlightIndexBound();
+
+            RefreshViewList();
+        }
+
+        private void HandleLeftArrow()
+        {
+            //in case user only want to move the cursor in the text box, not the page
+            //but user can still use the left key when searching for task in filter list
+            if (parseResult.CommandText == Command.Search || inputBox.Text == string.Empty)
+            {
+                MoveToPreviousPage();
+            }
+        }
+
+        private void HandleRightArrow()
+        {
+            //in case user only want to move the cursor in the text box, not the page
+            //but user can still use the right key when searching for task in filter list
+            if (parseResult.CommandText == Command.Search || inputBox.Text == string.Empty)
+            {
+                MoveToNextPage();
+            }
+        }
+        #endregion
+
+        #region Page Navigation Helper Methods
+        //@author A0088574M
+        //go to next page, by modifying listStartIndex and listEndIndex
+        //may modify highlightIndex
+        private void MoveToNextPage()
+        {
+            //already at the last page
+            if (listEndIndex == renderedTasks.Count)
+            {
+                return;
+            }
+
+            listEndIndex += NUMBER_OF_TASKS_DISPLAYED;
+            listStartIndex = listEndIndex - NUMBER_OF_TASKS_DISPLAYED;
+
+            CheckListIndexBound();
+            CheckHighlightIndexBound();
+
+            ResetSelection();
+
+            RefreshViewList();
+        }
+
+        //@author A0088574M
+        //go to previous page, by modifying listStartIndex and listEndIndex
+        //may modify highlightIndex
+        private void MoveToPreviousPage()
+        {
+            //already at the first page
+            if (listStartIndex == 0)
+            {
+                return;
+            }
+
+            listStartIndex -= NUMBER_OF_TASKS_DISPLAYED;
+            listEndIndex = listStartIndex + NUMBER_OF_TASKS_DISPLAYED;
+
+            CheckListIndexBound();
+            CheckHighlightIndexBound();
+
+            ResetSelection();
+
+            RefreshViewList();
+        }
+        #endregion
+
+        #region Page Number Methods
+        //@author A0083834Y
+        private int GetTotalPageNumber()
+        {
+            return (renderedTasks.Count - 1) / NUMBER_OF_TASKS_DISPLAYED + 1;
+        }
+
+        //@author A0088574M
+        private int GetCurrentPageNumber()
+        {
+            return (listEndIndex - 1) / NUMBER_OF_TASKS_DISPLAYED + 1;
+        }
+        #endregion
+
+        #region Highlight Methods
+        //@author A0092104U
+        private void StartHighlighting()
+        {
+            highlightListIndex = 0;
+            isHighlighting = true;
+            ResetSelection();
+        }
+
+        //@author A0092104U
+        private void StopHighlighting()
+        {
+            isHighlighting = false;
+            ResetSelection();
+        }
+
+        //@author A0092104U
+        private void ResetSelection()
+        {
+            // We have a non-ambiguous match iff there is exactly one task rendered.
+            // Otherwise, set the selectedTask to null to represent no task selected.
+            selectedTask = renderedTasks.Count == 1 ? renderedTasks[0] : null;
+        }
+
+        //@author A0088574M
+        //if the highLightIndex out of bound the set it back to the correct bound
+        private void CheckHighlightIndexBound()
+        {
+            if (highlightListIndex < 0)
+            {
+                highlightListIndex = 0;
+            }
+
+            if (highlightListIndex > (listEndIndex - 1) % NUMBER_OF_TASKS_DISPLAYED)
+            {
+                highlightListIndex = (listEndIndex - 1) % NUMBER_OF_TASKS_DISPLAYED;
+            }
+        }
+        #endregion
+
+        #region Display List Index Methods
+        //@author A0088574M
+        private void InitializeListBounderIndex()
+        {
+            listStartIndex = 0;
+
+            if (renderedTasks.Count > NUMBER_OF_TASKS_DISPLAYED)
+            {
+                listEndIndex = NUMBER_OF_TASKS_DISPLAYED;
+            }
+            else
+            {
+                listEndIndex = renderedTasks.Count;
+            }
+        }
+        
+        //@author A0088574M
+        //if the list index is out of bound then set it back to the correct bound
+        private void CheckListIndexBound()
+        {
+            if (listStartIndex < 0)
+            {
+                listStartIndex = 0;
+            }
+
+            if (listEndIndex > renderedTasks.Count)
+            {
+                listEndIndex = renderedTasks.Count;
+            }
+        }
+        #endregion
+    }
+}
